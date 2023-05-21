307a3d238da2abd2c0ebd25c8f16f92f43ec2c64
Pranav Gade <pranavgade20@gmail.com>
2023-05-21 18:36:00 +0100

    switching infra to callum's account

--- infrastructure/docker-provision.sh
+++ infrastructure/docker-provision.sh
@@ -15,23 +15,23 @@ if [ ! -f "$SSH_KEY" ]; then
     exit 1
 fi
 
-# Skip over the prompt to add the instance to known_hosts
-ssh-keyscan -H $1 >> ~/.ssh/known_hosts
+## Skip over the prompt to add the instance to known_hosts
+#ssh-keyscan -H $1 >> ~/.ssh/known_hosts
+#
+## Copy gituhb ssh key to the remote machine
+#scp arena_ssh ubuntu@$1:/home/ubuntu/.ssh/arena_ssh
+#
+## Install nvidia-container-toolkit and nvidia-container-runtime
+#ssh ubuntu@$1 'bash -s' < docker-provision-on-instance-part-1.sh
+#
+## The above will reboot the instance; wait until it's back up to finish
+#ssh -q ubuntu@$1 exit
+#until [ $? -eq 0 ]
+#  do
+#    echo 'Waiting until the instance has restarted...'
+#    sleep 2
+#    ssh -q ubuntu@$1 exit
+#  done
 
-# Copy gituhb ssh key to the remote machine
-scp arena_ssh ubuntu@$1:/home/ubuntu/.ssh/arena_ssh
-
-# Install nvidia-container-toolkit and nvidia-container-runtime
-ssh ubuntu@$1 'bash -s' < docker-provision-on-instance-part-1.sh 
-
-# The above will reboot the instance; wait until it's back up to finish
-ssh -q ubuntu@$1 exit
-until [ $? -eq 0 ]
-  do
-    echo 'Waiting until the instance has restarted...'
-    sleep 2
-    ssh -q ubuntu@$1 exit
-  done
-
-# Pull the wmlbredwood/wmlb image and start one or two containers
-# ssh ubuntu@$1 'bash -s' < docker-provision-on-instance-part-2.sh
+# Pull the docker image and start a container
+ ssh ubuntu@$1 'bash -s' < docker-provision-on-instance-part-2.sh
