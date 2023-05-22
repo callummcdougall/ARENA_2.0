f699beb42b1c5810e24a9eaded75e72bf196d073
callummcdougall <cal.s.mcdougall@gmail.com>
2023-05-22 08:17:41 +0100

    update homepages

--- chapter1_transformers/exercises/part6_othellogpt/tests.py
+++ chapter1_transformers/exercises/part6_othellogpt/tests.py
@@ -1,7 +1,9 @@
 import torch as t
 from torch import Tensor
-from typing import Callable
+import einops
+from typing import Callable, List
 from jaxtyping import Float
+from transformer_lens import HookedTransformer
 
 # FLAT SOLUTION
 # YOUR CODE HERE - define `blank_probe` and `my_probe`
@@ -35,3 +37,41 @@ def test_apply_scale(apply_scale: Callable):
         t.testing.assert_close(resid_expected, resid_actual)
 
     print("All tests in `test_apply_scale` passed!")
+
+
+def test_calculate_neuron_input_weights(
+    calculate_neuron_input_weights: Callable,
+    model: HookedTransformer, 
+) -> Float[Tensor, "neurons rows cols"]:
+    '''
+    Returns tensor of the input weights for each neuron in the list, at each square on the board,
+    projected along the corresponding probe directions.
+    '''
+    layer = 5
+    neuron = 1393
+    probe = t.randn(model.cfg.d_model, 8, 8, device=model.cfg.device)
+
+    w_in = model.W_in[layer, :, neuron].detach()
+    w_in_normed = w_in / w_in.norm(dim=0, keepdim=True)
+
+    expected = einops.einsum(
+        w_in_normed, probe,
+        "d_model, d_model row col -> row col",
+    )
+    actual = calculate_neuron_input_weights(model, probe, layer, neuron = 1393)
+
+    t.testing.assert_close(expected, actual)
+    print("All tests in `test_calculate_neuron_input_weights` passed!")
+
+
+
+def test_patching_metric(
+    patching_metric: Callable,
+    clean_log_probs: t.Tensor,
+    corrupted_log_probs: t.Tensor
+):
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
+    t.testing.assert_close(patching_metric(clean_log_probs), t.tensor(1.0).to(device))
+    t.testing.assert_close(patching_metric(corrupted_log_probs), t.tensor(0.0).to(device))
+    t.testing.assert_close(patching_metric(0.5 * (clean_log_probs + corrupted_log_probs)), t.tensor(0.5418457984924316).to(device))
+    print("All tests in `test_patching_metric` passed!")
