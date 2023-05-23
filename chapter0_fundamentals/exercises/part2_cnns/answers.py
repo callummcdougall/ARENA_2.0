0ccc4edcf926b5a262da73c8d0f9b7c5aa0190b0
ARENA Account <pranav@arena.education>
2023-05-23 14:42:09 +0000

    Finished convoluted convolution exercises

--- chapter0_fundamentals/exercises/part2_cnns/answers.py
+++ chapter0_fundamentals/exercises/part2_cnns/answers.py
@@ -262,3 +262,247 @@ def as_strided_mm(matA: Float[Tensor, "i j"], matB: Float[Tensor, "j k"]) -> Flo
 if MAIN:
     tests.test_mm(as_strided_mm)
     tests.test_mm2(as_strided_mm)
+
+# %%
+def conv1d_minimal_simple(x: Float[Tensor, "w"], weights: Float[Tensor, "kw"]) -> Float[Tensor, "ow"]:
+    '''
+    Like torch's conv1d using bias=False and all other keyword arguments left at their default values.
+
+    Simplifications: batch = input channels = output channels = 1.
+
+    x: shape (width,)
+    weights: shape (kernel_width,)
+
+    Returns: shape (output_width,)
+    '''
+    width = x.shape[0]
+    kernel_width = weights.shape[0]
+    output_width = width - kernel_width + 1
+
+    x_strided = x.as_strided(
+        size=(output_width, kernel_width),
+        stride=(x.stride(0), x.stride(0)),
+    )
+
+    return einops.einsum(x_strided, weights, "ow kw, kw -> ow")
+
+if MAIN:
+    tests.test_conv1d_minimal_simple(conv1d_minimal_simple)
+
+# %%
+def conv1d_minimal(x: Float[Tensor, "b ic w"], weights: Float[Tensor, "oc ic kw"]) -> Float[Tensor, "b oc ow"]:
+    '''
+    Like torch's conv1d using bias=False and all other keyword arguments left at their default values.
+
+    x: shape (batch, in_channels, width)
+    weights: shape (out_channels, in_channels, kernel_width)
+
+    Returns: shape (batch, out_channels, output_width)
+    '''
+    batch, input_channels, width = x.shape
+    output_channels, input_channels, kernel_width = weights.shape
+    output_width = width - kernel_width + 1
+
+    x_strided = x.as_strided(
+        size=(batch, input_channels, output_width, kernel_width),
+        stride=(x.stride(0), x.stride(1), x.stride(2), x.stride(2)),
+    )
+
+    return einops.einsum(x_strided, weights, "b ic ow kw, oc ic kw -> b oc ow")
+
+if MAIN:
+    tests.test_conv1d_minimal(conv1d_minimal)
+
+# %%
+def conv2d_minimal(x: Float[Tensor, "b ic h w"], weights: Float[Tensor, "oc ic kh kw"]) -> Float[Tensor, "b oc oh ow"]:
+    '''
+    Like torch's conv2d using bias=False and all other keyword arguments left at their default values.
+
+    x: shape (batch, in_channels, height, width)
+    weights: shape (out_channels, in_channels, kernel_height, kernel_width)
+
+    Returns: shape (batch, out_channels, output_height, output_width)
+    '''
+    batch, input_channels, height, width = x.shape
+    output_channels, input_channels, kernel_height, kernel_width = weights.shape
+    output_width = width - kernel_width + 1
+    output_height = height - kernel_height + 1
+
+    s_batch, s_input_channels, s_height, s_width = x.stride()
+
+    x_strided = x.as_strided(
+        size=(batch, input_channels, output_height, kernel_height, output_width, kernel_width),
+        stride=(s_batch, s_input_channels, s_height, s_height, s_width, s_width),
+    )
+
+    return einops.einsum(x_strided, weights, "b ic oh kh ow kw, oc ic kh kw -> b oc oh ow")
+
+
+if MAIN:
+    tests.test_conv2d_minimal(conv2d_minimal)
+
+# %%
+def pad1d(x: t.Tensor, left: int, right: int, pad_value: float) -> t.Tensor:
+    '''Return a new tensor with padding applied to the edges.
+
+    x: shape (batch, in_channels, width), dtype float32
+
+    Return: shape (batch, in_channels, left + right + width)
+    '''
+    batch, in_channels, width = x.shape
+    x_padded = x.new_full((batch, in_channels, left + right + width), pad_value)
+    x_padded[..., left:left+width] = x
+    return x_padded
+
+if MAIN:
+    tests.test_pad1d(pad1d)
+    tests.test_pad1d_multi_channel(pad1d)
+
+# %%
+def pad2d(x: t.Tensor, left: int, right: int, top: int, bottom: int, pad_value: float) -> t.Tensor:
+    '''Return a new tensor with padding applied to the edges.
+
+    x: shape (batch, in_channels, height, width), dtype float32
+
+    Return: shape (batch, in_channels, top + height + bottom, left + width + right)
+    '''
+    batch, in_channels, height, width = x.shape
+    x_padded = x.new_full(
+        size=(batch, in_channels, top + bottom + height, left + right + width),
+        fill_value=pad_value,
+    )
+    x_padded[..., top:top+height,left:left+width] = x
+    return x_padded
+
+if MAIN:
+    tests.test_pad2d(pad2d)
+    tests.test_pad2d_multi_channel(pad2d)
+
+# %%
+def conv1d(
+    x: Float[Tensor, "b ic w"], 
+    weights: Float[Tensor, "oc ic kw"], 
+    stride: int = 1, 
+    padding: int = 0
+) -> Float[Tensor, "b oc ow"]:
+    '''
+    Like torch's conv1d using bias=False.
+
+    x: shape (batch, in_channels, width)
+    weights: shape (out_channels, in_channels, kernel_width)
+
+    Returns: shape (batch, out_channels, output_width)
+    '''
+    batch, input_channels, width = x.shape
+    output_channels, input_channels, kernel_width = weights.shape
+    output_width = (width + 2 * padding - kernel_width) // stride + 1
+
+    x_padded = pad1d(x, padding, padding, 0)
+    s_batch, s_input_channels, s_width = x_padded.stride()
+    x_strided = x_padded.as_strided(
+        size=(batch, input_channels, output_width, kernel_width),
+        stride=(s_batch, s_input_channels, s_width * stride, s_width),
+    )
+
+    return einops.einsum(x_strided, weights, "b ic ow kw, oc ic kw -> b oc ow")
+
+if MAIN:
+    tests.test_conv1d(conv1d)
+
+
+# %%
+IntOrPair = Union[int, Tuple[int, int]]
+Pair = Tuple[int, int]
+
+def force_pair(v: IntOrPair) -> Pair:
+    '''Convert v to a pair of int, if it isn't already.'''
+    if isinstance(v, tuple):
+        if len(v) != 2:
+            raise ValueError(v)
+        return (int(v[0]), int(v[1]))
+    elif isinstance(v, int):
+        return (v, v)
+    raise ValueError(v)
+
+# Examples of how this function can be used:
+
+
+if MAIN:
+    for v in [(1, 2), 2, (1, 2, 3)]:
+        try:
+            print(f"{v!r:9} -> {force_pair(v)!r}")
+        except ValueError:
+            print(f"{v!r:9} -> ValueError")
+
+# %%
+def conv2d(
+    x: Float[Tensor, "b ic h w"], 
+    weights: Float[Tensor, "oc ic kh kw"], 
+    stride: IntOrPair = 1, 
+    padding: IntOrPair = 0
+) -> Float[Tensor, "b oc oh ow"]:
+    '''
+    Like torch's conv2d using bias=False
+
+    x: shape (batch, in_channels, height, width)
+    weights: shape (out_channels, in_channels, kernel_height, kernel_width)
+
+    Returns: shape (batch, out_channels, output_height, output_width)
+    '''
+    padding_height, padding_width = force_pair(padding)
+    stride_down, stride_right = force_pair(stride)
+
+    batch, input_channels, height, width = x.shape
+    output_channels, input_channels, kernel_height, kernel_width = weights.shape
+    output_height = (height + 2 * padding_height - kernel_height) // stride_down + 1
+    output_width = (width + 2 * padding_width - kernel_width) // stride_right + 1
+
+    x_padded = pad2d(x, padding_width, padding_width, padding_height, padding_height, 0)
+    s_batch, s_input_channels, s_height, s_width = x_padded.stride()
+    x_strided = x_padded.as_strided(
+        size=(batch, input_channels, output_height, kernel_height ,output_width, kernel_width),
+        stride=(s_batch, s_input_channels, s_height * stride_down, s_height, s_width * stride_right, s_width),
+    )
+
+    return einops.einsum(x_strided, weights, "b ic oh kh ow kw, oc ic kh kw -> b oc oh ow")
+
+
+if MAIN:
+    tests.test_conv2d(conv2d)
+
+# %%
+def maxpool2d(
+    x: Float[Tensor, "b ic h w"], 
+    kernel_size: IntOrPair, 
+    stride: Optional[IntOrPair] = None, 
+    padding: IntOrPair = 0
+) -> Float[Tensor, "b ic oh ow"]:
+    '''
+    Like PyTorch's maxpool2d.
+
+    x: shape (batch, channels, height, width)
+    stride: if None, should be equal to the kernel size
+
+    Return: (batch, channels, output_height, output_width)
+    '''
+    stride = kernel_size if not stride else stride
+    kernel_height, kernel_width = force_pair(kernel_size)
+    stride_down, stride_right = force_pair(stride)
+    padding_height, padding_width = force_pair(padding)
+
+    batch, input_channels, height, width = x.shape
+    output_height = (height + 2 * padding_height - kernel_height) // stride_down + 1
+    output_width = (width + 2 * padding_width - kernel_width) // stride_right + 1
+
+    x_padded = pad2d(x, padding_width, padding_width, padding_height, padding_height, -t.inf)
+    s_batch, s_input_channels, s_height, s_width = x_padded.stride()
+    x_strided = x_padded.as_strided(
+        size=(batch, input_channels, output_height, kernel_height ,output_width, kernel_width),
+        stride=(s_batch, s_input_channels, s_height * stride_down, s_height, s_width * stride_right, s_width),
+    )
+
+    return einops.reduce(x_strided, "b ic oh kh ow kw -> b ic oh ow", "max")
+
+
+if MAIN:
+    tests.test_maxpool2d(maxpool2d)
