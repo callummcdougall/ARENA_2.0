cb31666089bc51e615b4480dd1793281307acb3e
ARENA Account <pranav@arena.education>
2023-05-23 10:39:38 +0000

    einsum

--- chapter0_fundamentals/exercises/part2_cnns/answers.py
+++ chapter0_fundamentals/exercises/part2_cnns/answers.py
@@ -98,3 +98,44 @@ if MAIN:
 arr9 = einops.reduce(arr.astype(float), 'nums channels y x -> y (nums x)', 'min')
 if MAIN:
     display_array_as_img(arr9)
+
+
+# %%
+
+def einsum_trace(mat: np.ndarray):
+    '''
+    Returns the same as `np.trace`.
+    '''
+    return einops.einsum(mat, "i i -> ")
+
+def einsum_mv(mat: np.ndarray, vec: np.ndarray):
+    '''
+    Returns the same as `np.matmul`, when `mat` is a 2D array and `vec` is 1D.
+    '''
+    return einops.einsum(mat, vec, "i j, j -> i")
+
+def einsum_mm(mat1: np.ndarray, mat2: np.ndarray):
+    '''
+    Returns the same as `np.matmul`, when `mat1` and `mat2` are both 2D arrays.
+    '''
+    return einops.einsum(mat1, mat2, "i j, j k -> i k")
+
+def einsum_inner(vec1: np.ndarray, vec2: np.ndarray):
+    '''
+    Returns the same as `np.inner`.
+    '''
+    return einops.einsum(vec1, vec2, "i, i ->")
+
+def einsum_outer(vec1: np.ndarray, vec2: np.ndarray):
+    '''
+    Returns the same as `np.outer`.
+    '''
+    return einops.einsum(vec1, vec2, "i, j -> i j")
+
+
+if MAIN:
+    tests.test_einsum_trace(einsum_trace)
+    tests.test_einsum_mv(einsum_mv)
+    tests.test_einsum_mm(einsum_mm)
+    tests.test_einsum_inner(einsum_inner)
+    tests.test_einsum_outer(einsum_outer)
