e24e1fc56930ace1937439e264d520b938c742d8
ARENA Account <pranav@arena.education>
2023-05-23 16:37:59 +0000

    modules

--- chapter0_fundamentals/exercises/part2_cnns/answers.py
+++ chapter0_fundamentals/exercises/part2_cnns/answers.py
@@ -410,9 +410,119 @@ def pad2d(x: t.Tensor, left: int, right: int, top: int, bottom: int, pad_value: 
     return results
 
 
+
 if MAIN:
     tests.test_pad2d(pad2d)
     tests.test_pad2d_multi_channel(pad2d)
 
 # %%
 
+IntOrPair = Union[int, Tuple[int, int]]
+Pair = Tuple[int, int]
+
+from solutions import maxpool2d
+
+class MaxPool2d(nn.Module):
+    def __init__(self, kernel_size: IntOrPair, stride: Optional[IntOrPair] = None, padding: IntOrPair = 1):
+        super().__init__()
+        # SOLUTION
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''Call the functional version of maxpool2d.'''
+        # SOLUTION
+        return maxpool2d(x, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding)
+
+    def extra_repr(self) -> str:
+        '''Add additional information to the string representation of this class.'''
+        # SOLUTION
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["kernel_size", "stride", "padding"]])
+
+
+if MAIN:
+    tests.test_maxpool2d_module(MaxPool2d)
+    m = MaxPool2d(kernel_size=3, stride=2, padding=1)
+    print(f"Manually verify that this is an informative repr: {m}")
+
+# %%
+
+class ReLU(nn.Module):
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return x * ( x > 0)
+
+
+if MAIN:
+    tests.test_relu(ReLU)
+
+# %%
+class Flatten(nn.Module):
+    def __init__(self, start_dim: int = 1, end_dim: int = -1) -> None:
+        super().__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, input: t.Tensor) -> t.Tensor:
+        '''
+        Flatten out dimensions from start_dim to end_dim, inclusive of both.
+        '''
+
+        shape = input.shape
+
+        start_dim = self.start_dim
+        end_dim = self.end_dim if self.end_dim >= 0 else len(shape) + self.end_dim
+
+        shape_left = shape[:start_dim]
+        # shape_middle = t.prod(t.tensor(shape[start_dim : end_dim+1])).item()
+        shape_middle = functools.reduce(lambda x, y: x*y, shape[start_dim : end_dim+1])
+        shape_right = shape[end_dim+1:]
+
+        new_shape = shape_left + (shape_middle,) + shape_right
+
+        return t.reshape(input, new_shape)
+        
+
+    def extra_repr(self) -> str:
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["start_dim", "end_dim"]])
+
+
+
+if MAIN:
+    print(Flatten(0, -1).forward(t.tensor([[1,2,3],[2,3,4]])))
+    tests.test_flatten(Flatten)
+
+# %%
+class Linear(nn.Module):
+    def __init__(self, in_features: int, out_features: int, bias=True):
+        '''
+        A simple linear (technically, affine) transformation.
+
+        The fields should be named `weight` and `bias` for compatibility with PyTorch.
+        If `bias` is False, set `self.bias` to None.
+        '''
+        super().__init__()
+        self.weight = nn.Parameter(t.rand(out_features, in_features))
+        self.bias = None
+        if bias:
+            self.bias = nn.Parameter(t.rand(out_features))
+        
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        x: shape (*, in_features)
+        Return: shape (*, out_features)
+        '''
+        mul = t.mm(x,self.weight.T)
+        if self.bias is not None:
+            mul += self.bias
+        return mul
+
+    def extra_repr(self) -> str:
+        pass
+
+
+if MAIN:
+    tests.test_linear_forward(Linear)
+    tests.test_linear_parameters(Linear)
+    tests.test_linear_no_bias(Linear)
