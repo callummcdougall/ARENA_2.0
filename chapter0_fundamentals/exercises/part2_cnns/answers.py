c756f2d2cdfe8be2f1fe4dbfca637c3922775469
ARENA Account <pranav@arena.education>
2023-05-23 15:58:59 +0000

    Implement convolution (first try)

--- chapter0_fundamentals/exercises/part2_cnns/answers.py
+++ chapter0_fundamentals/exercises/part2_cnns/answers.py
@@ -506,3 +506,131 @@ def maxpool2d(
 
 if MAIN:
     tests.test_maxpool2d(maxpool2d)
+
+class MaxPool2d(nn.Module):
+    def __init__(self, kernel_size: IntOrPair, stride: Optional[IntOrPair] = None, padding: IntOrPair = 1):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''Call the functional version of maxpool2d.'''
+        return maxpool2d(x, self.kernel_size, self.stride, self.padding)
+
+    def extra_repr(self) -> str:
+        '''Add additional information to the string representation of this class.'''
+        attrs = [k for k in self.__dict__ if not k.startswith("_")]
+        return ", ".join([f"{key}={getattr(self, key)}" for key in attrs])
+
+
+if MAIN:
+    tests.test_maxpool2d_module(MaxPool2d)
+    m = MaxPool2d(kernel_size=3, stride=2, padding=1)
+    print(f"Manually verify that this is an informative repr: {m}")
+# %%
+
+class ReLU(nn.Module):
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return t.maximum(t.tensor(0), x)
+
+
+if MAIN:
+    tests.test_relu(ReLU)
+
+# %%
+class Flatten(nn.Module):
+    def __init__(self, start_dim: int = 1, end_dim: int = -1) -> None:
+        super().__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, input: t.Tensor) -> t.Tensor:
+        '''
+        Flatten out dimensions from start_dim to end_dim, inclusive of both.
+        '''
+        shape = input.shape
+        start_dim = self.start_dim
+        end_dim = self.end_dim if self.end_dim >= 0 else len(shape) + self.end_dim
+
+
+        left = shape[:start_dim]
+        right = shape[end_dim+1:]
+        mid = functools.reduce(lambda x, y: x*y, shape[start_dim : end_dim+1])
+
+        return input.reshape(left + (mid,) + right)
+
+
+    def extra_repr(self) -> str:
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["start_dim", "end_dim"]])
+        
+if MAIN:
+    tests.test_flatten(Flatten)
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features: int, out_features: int, bias=True):
+        '''
+        A simple linear (technically, affine) transformation.
+
+        The fields should be named `weight` and `bias` for compatibility with PyTorch.
+        If `bias` is False, set `self.bias` to None.
+        '''
+        super().__init__()
+        normalize = lambda x: (x - 0.5) * 2 / t.tensor(in_features).sqrt()
+        self.weight = nn.Parameter(normalize(t.rand(out_features, in_features)))
+        self.bias = nn.Parameter(normalize(t.rand(out_features))) if bias else None
+
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        x: shape (*, in_features)
+        Return: shape (*, out_features)
+        '''
+        x = x @ self.weight.T
+        if self.bias is not None:
+            x = x + self.bias
+
+        return x
+
+    def extra_repr(self) -> str:
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["start_dim", "end_dim"]])
+
+
+if MAIN:
+    tests.test_linear_forward(Linear)
+    tests.test_linear_parameters(Linear)
+    tests.test_linear_no_bias(Linear)
+
+
+class Conv2d(nn.Module):
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: IntOrPair, stride: IntOrPair = 1, padding: IntOrPair = 0
+    ):
+        '''
+        Same as torch.nn.Conv2d with bias=False.
+
+        Name your weight field `self.weight` for compatibility with the PyTorch version.
+        '''
+        super().__init__()
+        kernel_height, kernel_width = force_pair(kernel_size)
+        k = in_channels * kernel_height * kernel_width
+        normalize = lambda x: (x - 0.5) * 2 / t.tensor(k).sqrt()
+        self.weight = nn.Parameter(
+            normalize(
+                t.rand(out_channels, in_channels, kernel_height, kernel_width)
+            )
+        )
+        self.stride = stride
+        self.padding = padding
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''Apply the functional conv2d you wrote earlier.'''
+        return conv2d(x, self.weight, self.stride, self.padding)
+
+    def extra_repr(self) -> str:
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["start_dim", "end_dim"]])
+
+
+if MAIN:
+    tests.test_conv2d_module(Conv2d)
