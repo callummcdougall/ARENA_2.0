f372f97b98dde46b71d9a189836d3d7129edf5ea
ARENA Account <pranav@arena.education>
2023-05-24 11:07:16 +0000

    Implement validation_step

---
+++ chapter0_fundamentals/exercises/part3_resnets/answers.py
@@ -0,0 +1,173 @@
+# %%
+import os
+
+from pytorch_lightning.utilities.types import STEP_OUTPUT; os.environ["ACCELERATE_DISABLE_RICH"] = "1"
+import sys
+import torch as t
+from torch import Tensor
+from torch import nn
+import torch.nn.functional as F
+from collections import OrderedDict
+from einops import rearrange
+from dataclasses import dataclass
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader, Subset
+from tqdm.notebook import tqdm
+from typing import Any, List, Optional, Tuple, Dict
+from PIL import Image
+from IPython.display import display
+from pathlib import Path
+import torchinfo
+import json
+import pandas as pd
+from jaxtyping import Float, Int
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
+
+# Make sure exercises are in the path
+chapter = r"chapter0_fundamentals"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part3_resnets"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+os.chdir(section_dir)
+
+from part2_cnns.solutions import get_mnist, Linear, Conv2d, Flatten, ReLU, MaxPool2d
+from part3_resnets.utils import print_param_count
+import part3_resnets.tests as tests
+from plotly_utils import line, plot_train_loss_and_test_accuracy_from_metrics
+
+device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+
+MAIN = __name__ == "__main__"
+# %%
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.blocks = nn.Sequential(
+            Conv2d(1, 32, kernel_size=3, padding=1),
+            ReLU(),
+            MaxPool2d(2, stride=2, padding=0),
+            Conv2d(32, 64, kernel_size=3, padding=1),
+            ReLU(),
+            MaxPool2d(2, stride=2, padding=0),
+            Flatten(),
+            Linear(64*7*7, 128),
+            Linear(128, 10)
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return self.blocks(x)
+
+
+if MAIN:
+    model = ConvNet()
+    print(model)
+# %%
+# %%
+if MAIN:
+    summary = torchinfo.summary(model, input_size=(1, 1, 28, 28))
+    print(summary)
+# %%
+if MAIN:
+    MNIST_TRANSFORM = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+def get_mnist(subset: int = 1):
+    '''Returns MNIST training data, sampled by the frequency given in `subset`.'''
+    mnist_trainset = datasets.MNIST(root="./data", train=True, download=True, transform=MNIST_TRANSFORM)
+    mnist_testset = datasets.MNIST(root="./data", train=False, download=True, transform=MNIST_TRANSFORM)
+
+    if subset > 1:
+        mnist_trainset = Subset(mnist_trainset, indices=range(0, len(mnist_trainset), subset))
+        mnist_testset = Subset(mnist_testset, indices=range(0, len(mnist_testset), subset))
+
+    return mnist_trainset, mnist_testset
+
+
+
+if MAIN:
+    mnist_trainset, mnist_testset = get_mnist()
+    mnist_trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+    mnist_testloader = DataLoader(mnist_testset, batch_size=64, shuffle=False)
+# %%
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
+
+# %%
+class LitConvNet(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.convnet = ConvNet()
+
+    def training_step(self, batch: Tuple[t.Tensor, t.Tensor], batch_idx: int) -> t.Tensor:
+        '''
+        Here you compute and return the training loss and some additional metrics for e.g. the progress bar or logger.
+        '''
+        imgs, labels = batch
+        logits = self.convnet(imgs)
+        loss = F.cross_entropy(logits, labels)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch: Tuple[t.Tensor, t.Tensor], batch_idx: int) -> None:
+        imgs, labels = batch
+        logits = self.convnet(imgs) # [batch, 10]
+        probs = logits.softmax(dim=-1)
+        predictions = probs.argmax(dim=-1) # [batch]
+        accuracy = t.sum(predictions == labels)
+        self.log("accuracy", accuracy)
+
+    def configure_optimizers(self):
+        '''
+        Choose what optimizers and learning-rate schedulers to use in your optimization.
+        '''
+        optimizer = t.optim.Adam(self.parameters())
+        return optimizer
+
+# %%
+# Set batch size
+
+if MAIN:
+    batch_size = 64
+    max_epochs = 3
+
+    # Create the model & training system
+    model = LitConvNet()
+
+    # Get dataloaders
+    trainset, testset = get_mnist(subset = 10)
+    trainloader = DataLoader(trainset, shuffle=True, batch_size=batch_size)
+    testloader = DataLoader(testset, shuffle=True, batch_size=batch_size)
+
+    # Get a logger, to record metrics during training
+    logger = CSVLogger(save_dir=os.getcwd() + "/logs", name="day4-convenet")
+
+    # Train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    trainer = pl.Trainer(
+        max_epochs=max_epochs,
+        logger=logger,
+        log_every_n_steps=1,
+    )
+    trainer.fit(model=model, train_dataloaders=trainloader, val_dataloaders=testloader)
+# %%
+
+if MAIN:
+    metrics = pd.read_csv(f"{trainer.logger.log_dir}/metrics.csv")
+
+    print(metrics.head())
+# %%
+
+if MAIN:
+    line(
+        metrics["train_loss"].values,
+        x=metrics["step"].values,
+        yaxis_range=[0, metrics["train_loss"].max() + 0.1],
+        labels={"x": "Batches seen", "y": "Cross entropy loss"},
+        title="ConvNet training on MNIST",
+        width=800,
+        hovermode="x unified",
+        template="ggplot2", # alternative aesthetic for your plots (-:
+    )
+# %%
