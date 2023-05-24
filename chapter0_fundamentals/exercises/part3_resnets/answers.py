054c68637754d00b5006aec813f112ddb2a9168e
ARENA Account <pranav@arena.education>
2023-05-24 14:32:39 +0000

    ResNet implementation

--- chapter0_fundamentals/exercises/part3_resnets/answers.py
+++ chapter0_fundamentals/exercises/part3_resnets/answers.py
@@ -65,11 +65,6 @@ class ConvNet(nn.Module):
 
 # %%
 if MAIN:
-    summary = torchinfo.summary(model, input_size=(1, 1, 28, 28))
-    print(summary)
-
-# %%
-if MAIN:
     MNIST_TRANSFORM = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
@@ -298,7 +293,7 @@ class LitConvNetTest(pl.LightningModule):
         logits, labels, loss = self._shared_train_val_step(batch)
         acc = (logits.argmax(dim=-1) == labels).float().mean()
         self.log("val_loss", loss)
-        self.log("val_acc", acc)
+        self.log("accuracy", acc)
 
     def configure_optimizers(self):
         '''
@@ -319,3 +314,282 @@ if MAIN:
         log_every_n_steps=args.log_every_n_steps
     )
     trainer.fit(model=model, train_dataloaders=args.trainloader, val_dataloaders=args.testloader)
+
+#%%
+if MAIN:
+    metrics = pd.read_csv(f"{trainer.logger.log_dir}/metrics.csv")
+
+    plot_train_loss_and_test_accuracy_from_metrics(metrics, "Training ConvNet on MNIST data")
+
+
+#%%
+
+class BatchNorm2d(nn.Module):
+    # The type hints below aren't functional, they're just for documentation
+    running_mean: Float[Tensor, "num_features"]
+    running_var: Float[Tensor, "num_features"]
+    num_batches_tracked: Int[Tensor, ""] # This is how we denote a scalar tensor
+
+    def __init__(self, num_features: int, eps=1e-05, momentum=0.1):
+        '''
+        Like nn.BatchNorm2d with track_running_stats=True and affine=True.
+
+        Name the learnable affine parameters `weight` and `bias` in that order.
+        '''
+        super().__init__()
+        self.weight = nn.Parameter(t.ones(num_features))
+        self.bias = nn.Parameter(t.zeros(num_features)) 
+        self.register_buffer("running_mean", t.zeros(num_features))
+        self.register_buffer("running_var", t.ones(num_features))
+        self.register_buffer("num_batches_tracked", t.tensor(0))
+        self.momentum = momentum
+        self.eps = eps
+        self.num_features = num_features
+
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        Normalize each channel.
+
+        Compute the variance using `torch.var(x, unbiased=False)`
+        Hint: you may also find it helpful to use the argument `keepdim`.
+
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, channels, height, width)
+        '''
+        if self.training:
+            batch_mean = x.mean(dim=[0, 2, 3])
+            batch_var = x.var(dim=[0, 2, 3], unbiased=False)
+            scale = t.sqrt(batch_var + self.eps)
+            self.running_mean = self.momentum*batch_mean + (1 - self.momentum)*self.running_mean
+            self.running_var = self.momentum*batch_var + (1 - self.momentum)*self.running_var
+            x = (x - batch_mean[:, None, None])/scale[:, None, None]
+            self.num_batches_tracked += 1
+        else:
+            scale = t.sqrt(self.running_var + self.eps)
+            x = (x - self.running_mean[:, None, None])/scale[:, None, None]
+            
+        return self.weight[:, None, None]*x + self.bias[:, None, None]
+
+
+    def extra_repr(self) -> str:
+        attrs = [k for k in self.__dict__ if not k.startswith("_")]
+        return ", ".join([f"{key}={getattr(self, key)}" for key in attrs])
+
+
+if MAIN:
+    tests.test_batchnorm2d_module(BatchNorm2d)
+    tests.test_batchnorm2d_forward(BatchNorm2d)
+    tests.test_batchnorm2d_running_mean(BatchNorm2d)
+
+# %%
+
+class AveragePool(nn.Module):
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, channels)
+        '''
+        return x.mean(dim=[-2, -1])
+    
+#%%
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_feats: int, out_feats: int, first_stride=1):
+        '''
+        A single residual block with optional downsampling.
+
+        For compatibility with the pretrained model, declare the left side branch first using a `Sequential`.
+
+        If first_stride is > 1, this means the optional (conv + bn) should be present on the right branch. Declare it second using another `Sequential`.
+        '''
+        super().__init__()
+        self.left_branch = nn.Sequential(
+            Conv2d(in_feats, out_feats, kernel_size=3, stride=first_stride, padding=1),
+            BatchNorm2d(out_feats),
+            ReLU(),
+            Conv2d(out_feats, out_feats, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(out_feats),
+        )
+        if first_stride == 1:
+            self.right_branch = nn.Identity()
+        else:
+            self.right_branch = nn.Sequential(
+                Conv2d(in_feats, out_feats, kernel_size=1, stride=first_stride),
+                BatchNorm2d(out_feats)
+            )
+        self.last_relu = ReLU()
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        Compute the forward pass.
+
+        x: shape (batch, in_feats, height, width)
+
+        Return: shape (batch, out_feats, height / stride, width / stride)
+
+        If no downsampling block is present, the addition should just add the left branch's output to the input.
+        '''
+        return self.last_relu(self.left_branch(x) + self.right_branch(x))
+
+
+
+#%%
+
+class BlockGroup(nn.Module):
+    def __init__(self, n_blocks: int, in_feats: int, out_feats: int, first_stride=1):
+        '''An n_blocks-long sequence of ResidualBlock where only the first block uses the provided stride.'''
+        super().__init__()
+        self.block_group = nn.Sequential(
+            ResidualBlock(in_feats, out_feats, first_stride),
+            *[ResidualBlock(out_feats, out_feats, first_stride=1) for _ in range(n_blocks-1)]
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        Compute the forward pass.
+
+        x: shape (batch, in_feats, height, width)
+
+        Return: shape (batch, out_feats, height / first_stride, width / first_stride)
+        '''
+        return self.block_group(x)
+
+#%%
+
+class ResNet34(nn.Module):
+    def __init__(
+        self,
+        n_blocks_per_group=[3, 4, 6, 3],
+        out_features_per_group=[64, 128, 256, 512],
+        first_strides_per_group=[1, 2, 2, 2],
+        n_classes=1000,
+    ):
+        super().__init__()
+        block_params = zip(
+            n_blocks_per_group,
+            [64] + out_features_per_group,
+            out_features_per_group,
+            first_strides_per_group,
+        )
+        self.model = nn.Sequential(
+            Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=1),
+            BatchNorm2d(num_features=64),
+            ReLU(),
+            MaxPool2d(kernel_size=3, stride=2, padding=1),
+            *[BlockGroup(*params) for params in block_params],
+            AveragePool(),
+            Flatten(),
+            Linear(out_features_per_group[-1], n_classes)
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, n_classes)
+        '''
+        return self.model(x)
+
+
+if MAIN:
+    my_resnet = ResNet34()
+
+# %%
+def copy_weights(my_resnet: ResNet34, pretrained_resnet: models.resnet.ResNet) -> ResNet34:
+    '''Copy over the weights of `pretrained_resnet` to your resnet.'''
+
+    # Get the state dictionaries for each model, check they have the same number of parameters & buffers
+    mydict = my_resnet.state_dict()
+    pretraineddict = pretrained_resnet.state_dict()
+    assert len(mydict) == len(pretraineddict), "Mismatching state dictionaries."
+
+    # Define a dictionary mapping the names of your parameters / buffers to their values in the pretrained model
+    state_dict_to_load = {
+        mykey: pretrainedvalue
+        for (mykey, myvalue), (pretrainedkey, pretrainedvalue) in zip(mydict.items(), pretraineddict.items())
+    }
+
+    # Load in this dictionary to your model
+    my_resnet.load_state_dict(state_dict_to_load)
+
+    return my_resnet
+
+
+
+if MAIN:
+    pretrained_resnet = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
+    my_resnet = copy_weights(my_resnet, pretrained_resnet)
+
+#%%
+if MAIN:
+    print_param_count(my_resnet, pretrained_resnet)
+
+#%%
+if MAIN:
+    IMAGE_FILENAMES = [
+        "chimpanzee.jpg",
+        "golden_retriever.jpg",
+        "platypus.jpg",
+        "frogs.jpg",
+        "fireworks.jpg",
+        "astronaut.jpg",
+        "iguana.jpg",
+        "volcano.jpg",
+        "goofy.jpg",
+        "dragonfly.jpg",
+    ]
+
+    IMAGE_FOLDER = section_dir / "resnet_inputs"
+
+    images = [Image.open(IMAGE_FOLDER / filename) for filename in IMAGE_FILENAMES]
+
+IMAGE_SIZE = 224
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+IMAGENET_TRANSFORM = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE), antialias=True),
+    transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+])
+
+#%%
+def prepare_data(images: List[Image.Image]) -> t.Tensor:
+    '''
+    Return: shape (batch=len(images), num_channels=3, height=224, width=224)
+    '''
+    processed_images = [IMAGENET_TRANSFORM(image) for image in images]
+    return t.stack(processed_images)
+
+
+if MAIN:
+    prepared_images = prepare_data(images)
+
+    assert prepared_images.shape == (len(images), 3, IMAGE_SIZE, IMAGE_SIZE)
+
+#%%
+def predict(model, images):
+    logits: t.Tensor = model(images)
+    probs = logits.softmax(dim=-1)
+    return logits.argmax(dim=1), probs
+
+#%%
+if MAIN:
+    with open(section_dir / "imagenet_labels.json") as f:
+        imagenet_labels = list(json.load(f).values())
+
+if MAIN:
+    my_predictions, my_probs = predict(my_resnet, prepared_images)
+    pretrained_predictions, pretrained_probs = predict(pretrained_resnet, prepared_images)
+    assert all(my_predictions == pretrained_predictions)
+
+
+#%%
+# Print out your predictions, next to the corresponding images
+
+if MAIN:
+    for img, label, prob in zip(images, my_predictions, my_probs):
+        prob = prob.max(dim=-1).values.item()
+        print(f"Class {label}: {imagenet_labels[label]} ({prob=:.4f})")
+        display(img)
+        print()
