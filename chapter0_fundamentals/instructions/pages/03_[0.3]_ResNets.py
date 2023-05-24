aedefe62ac6d80246e119631e99dbac16b61ed4b
callummcdougall <cal.s.mcdougall@gmail.com>
2023-05-24 12:02:54 +0100

    update data aug

--- chapter0_fundamentals/instructions/pages/03_[0.3]_ResNets.py
+++ chapter0_fundamentals/instructions/pages/03_[0.3]_ResNets.py
@@ -1020,18 +1020,16 @@ Now we can update the `get_mnist` function to incorporate data augmentation for 
 
 ```python
 def get_mnist_augmented(subset: int = 1, train_transform=None, test_transform=None):
-
-if MAIN:
-       if train_transform is None:
-           train_transform = MNIST_TRANSFORM
-       if test_transform is None:
-           test_transform = MNIST_TRANSFORM
-       mnist_trainset = datasets.MNIST(root="./data", train=True, download=True, transform=train_transform)
-       mnist_testset = datasets.MNIST(root="./data", train=False, download=True, transform=test_transform)
-       if subset > 1:
-           mnist_trainset = Subset(mnist_trainset, indices=range(0, len(mnist_trainset), subset))
-           mnist_testset = Subset(mnist_testset, indices=range(0, len(mnist_testset), subset))
-       return mnist_trainset, mnist_testset
+    if train_transform is None:
+        train_transform = MNIST_TRANSFORM
+    if test_transform is None:
+        test_transform = MNIST_TRANSFORM
+    mnist_trainset = datasets.MNIST(root="./data", train=True, download=True, transform=data_augmentation_transform)
+    mnist_testset = datasets.MNIST(root="./data", train=False, download=True, transform=data_augmentation_transform)
+    if subset > 1:
+        mnist_trainset = Subset(mnist_trainset, indices=range(0, len(mnist_trainset), subset))
+        mnist_testset = Subset(mnist_testset, indices=range(0, len(mnist_testset), subset))
+    return mnist_trainset, mnist_testset
 
 ```
 
