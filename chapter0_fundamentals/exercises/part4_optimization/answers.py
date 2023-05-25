73581df8ba3c607041cab366edbfb6027dbb97e1
ARENA Account <pranav@arena.education>
2023-05-25 10:48:57 +0000

    add SGD

--- chapter0_fundamentals/exercises/part4_optimization/answers.py
+++ chapter0_fundamentals/exercises/part4_optimization/answers.py
@@ -1,13 +1,15 @@
 # %%
-import os; os.environ["ACCELERATE_DISABLE_RICH"] = "1"
+import os
+
+os.environ["ACCELERATE_DISABLE_RICH"] = "1"
 import sys
 import pandas as pd
 import torch as t
-from torch import optim
+from torch import Tensor, optim
 import torch.nn.functional as F
 from torchvision import datasets
 from torch.utils.data import DataLoader, Subset
-from typing import Callable, Iterable, Tuple, Optional
+from typing import Callable, Iterable, List, Tuple, Optional
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import CSVLogger, WandbLogger
 from dataclasses import dataclass
@@ -19,17 +21,23 @@ from IPython.display import display, HTML
 chapter = r"chapter0_fundamentals"
 exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
 section_dir = exercises_dir / "part4_optimization"
-if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
 os.chdir(section_dir)
 
 from plotly_utils import bar, imshow
-from part3_resnets.solutions import IMAGENET_TRANSFORM, get_resnet_for_feature_extraction, plot_train_loss_and_test_accuracy_from_metrics
+from part3_resnets.solutions import (
+    IMAGENET_TRANSFORM,
+    get_resnet_for_feature_extraction,
+    plot_train_loss_and_test_accuracy_from_metrics,
+)
 from part4_optimization.utils import plot_fn, plot_fn_with_points
 import part4_optimization.tests as tests
 
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
 
 MAIN = __name__ == "__main__"
+
 
 # %%
 def pathological_curve_loss(x: t.Tensor, y: t.Tensor):
@@ -41,9 +49,13 @@ def pathological_curve_loss(x: t.Tensor, y: t.Tensor):
 
 if MAIN:
     plot_fn(pathological_curve_loss)
+
+
 # %%
-def opt_fn_with_sgd(fn: Callable, xy: t.Tensor, lr=0.001, momentum=0.98, n_iters: int = 100):
-    '''
+def opt_fn_with_sgd(
+    fn: Callable, xy: t.Tensor, lr=0.001, momentum=0.98, n_iters: int = 100
+):
+    """
     Optimize the a given function starting from the specified point.
 
     xy: shape (2,). The (x, y) starting point.
@@ -51,12 +63,12 @@ def opt_fn_with_sgd(fn: Callable, xy: t.Tensor, lr=0.001, momentum=0.98, n_iters
     lr, momentum: parameters passed to the torch.optim.SGD optimizer.
 
     Return: (n_iters, 2). The (x,y) BEFORE each step. So out[0] is the starting point.
-    '''
+    """
     results = []
     optim = t.optim.SGD([xy], lr=lr, momentum=momentum)
 
     for i in range(n_iters):
-        optim.param_groups[0]['momentum'] = momentum ** (i / n_iters)
+        optim.param_groups[0]["momentum"] = momentum ** (i / n_iters)
         results.append(xy.tolist())
         val = fn(*xy)
         val.backward()
@@ -64,6 +76,7 @@ def opt_fn_with_sgd(fn: Callable, xy: t.Tensor, lr=0.001, momentum=0.98, n_iters
         optim.zero_grad()
 
     return t.tensor(results)
+
 
 if MAIN:
     xy = t.tensor([2.5, 2.5], requires_grad=True)
@@ -80,12 +93,20 @@ if MAIN:
         (optim.SGD, {"lr": 0.06, "momentum": 0.5}),
         (optim.SGD, {"lr": 0.06, "momentum": 0.5}),
     ]
-    optimizer_list += [(optim.SGD, {"lr": 0.4, "momentum": 0.9 + x/1000}) for x in range(0, 101, 10)]
+    optimizer_list += [
+        (optim.SGD, {"lr": 0.4, "momentum": 0.9 + x / 1000}) for x in range(0, 101, 10)
+    ]
     n_iters = 500
 
     for optimizer_class, params in optimizer_list:
         xy = t.tensor([2.5, 2.5], requires_grad=True)
-        xys = opt_fn_with_sgd(pathological_curve_loss, xy=xy, lr=params['lr'], momentum=params['momentum'], n_iters=n_iters)
+        xys = opt_fn_with_sgd(
+            pathological_curve_loss,
+            xy=xy,
+            lr=params["lr"],
+            momentum=params["momentum"],
+            n_iters=n_iters,
+        )
 
         points.append((xys, optimizer_class, params))
 
@@ -95,4 +116,61 @@ if MAIN:
 for x in points:
     print(x[0][-1])
 
+
 # %% Plot the evolution of momentum
+class SGD:
+    def __init__(
+        self,
+        params: Iterable[t.nn.parameter.Parameter],
+        lr: float,
+        momentum: float = 0.0,
+        weight_decay: float = 0.0,
+    ):
+        """Implements SGD with momentum.
+
+        Like the PyTorch version, but assume nesterov=False, maximize=False, and dampening=0
+            https://pytorch.org/docs/stable/generated/torch.optim.SGD.html#torch.optim.SGD
+
+        """
+        self.params = list(
+            params
+        )  # turn params into a list (because it might be a generator)
+        self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+
+        self.running_momentums: List[Optional[Tensor]] = [None] * len(self.params)
+
+    def zero_grad(self) -> None:
+        for param in self.params:
+            param.grad = None
+
+    @t.inference_mode()
+    def step(self) -> None:
+        for i, param in enumerate(self.params):
+            if self.weight_decay != 0:
+                grad = param.grad + self.weight_decay * param
+            else:
+                grad = param.grad
+            assert grad is not None
+
+            if self.momentum != 0:
+                if self.running_momentums[i] is None:
+                    self.running_momentums[i] = grad  # clone?
+                else:
+                    self.running_momentums[i].set_(
+                        self.momentum * self.running_momentums[i] + grad
+                    )
+                grad = self.running_momentums[i]
+            param.add_(grad, alpha=-self.lr)
+
+    def __repr__(self) -> str:
+        return f"SGD(lr={self.lr}, momentum={self.mu}, weight_decay={self.lmda})"
+
+
+if MAIN:
+    tests.test_sgd(SGD)
+
+
+#%%
+t.optim.SGD
