c2e6aae3ce7450b58301d83c12720fc2126f4e72
ARENA Account <pranav@arena.education>
2023-05-26 07:00:13 +0000

    setup

---
+++ chapter0_fundamentals/exercises/part5_backprop/answers.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import re
+import time
+import torch as t
+import numpy as np
+from pathlib import Path
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any, Callable, Iterator, Iterable, Optional, Union, Dict, List, Tuple
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+Arr = np.ndarray
+grad_tracking_enabled = True
+
+# Make sure exercises are in the path
+chapter = r"chapter0_fundamentals"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part5_backprop"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+os.chdir(section_dir)
+
+import part5_backprop.tests as tests
+from part5_backprop.utils import visualize, get_mnist
+from plotly_utils import line
+
+MAIN = __name__ == "__main__"
