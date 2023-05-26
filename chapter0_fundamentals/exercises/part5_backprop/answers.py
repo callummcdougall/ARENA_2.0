761fb0ca86e210965218dcca85845a5b9324e65a
ARENA Account <pranav@arena.education>
2023-05-26 15:14:38 +0000

    backprop

--- chapter0_fundamentals/exercises/part5_backprop/answers.py
+++ chapter0_fundamentals/exercises/part5_backprop/answers.py
@@ -19,7 +19,8 @@ grad_tracking_enabled = True
 chapter = r"chapter0_fundamentals"
 exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
 section_dir = exercises_dir / "part5_backprop"
-if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
 os.chdir(section_dir)
 
 import part5_backprop.tests as tests
@@ -28,35 +29,37 @@ from plotly_utils import line
 
 MAIN = __name__ == "__main__"
 
+
 # %%
 def log_back(grad_out: Arr, out: Arr, x: Arr) -> Arr:
-    '''Backwards function for f(x) = log(x)
+    """Backwards function for f(x) = log(x)
 
     grad_out: Gradient of some loss wrt out
     out: the output of np.log(x).
     x: the input of np.log.
 
     Return: gradient of the given loss wrt x
-    '''
-    return grad_out/x
+    """
+    return grad_out / x
 
 
 if MAIN:
     tests.test_log_back(log_back)
 
+
 # %%
 def unbroadcast(broadcasted: Arr, original: Arr) -> Arr:
-    '''
+    """
     Sum 'broadcasted' until it has the shape of 'original'.
 
     broadcasted: An array that was formerly of the same shape of 'original' and was expanded by broadcasting rules.
-    '''
+    """
     orig_shape = original.shape
-    while (len(orig_shape) < len(broadcasted.shape)):
+    while len(orig_shape) < len(broadcasted.shape):
         orig_shape = (1,) + orig_shape
     dims = []
     for idx, (orig_dim, b_dim) in enumerate(zip(orig_shape, broadcasted.shape)):
-        if (orig_dim != b_dim):
+        if orig_dim != b_dim:
             dims.append(idx)
     return np.reshape(np.sum(broadcasted, axis=tuple(dims)), original.shape)
 
@@ -64,15 +67,17 @@ def unbroadcast(broadcasted: Arr, original: Arr) -> Arr:
 if MAIN:
     tests.test_unbroadcast(unbroadcast)
 
+
 # %%
 def multiply_back0(grad_out: Arr, out: Arr, x: Arr, y: Union[Arr, float]) -> Arr:
-    '''Backwards function for x * y wrt argument 0 aka x.'''
+    """Backwards function for x * y wrt argument 0 aka x."""
     if not isinstance(y, Arr):
         y = np.array(y)
-    return unbroadcast(y*grad_out, x)
+    return unbroadcast(y * grad_out, x)
+
 
 def multiply_back1(grad_out: Arr, out: Arr, x: Union[Arr, float], y: Arr) -> Arr:
-    '''Backwards function for x * y wrt argument 1 aka y.'''
+    """Backwards function for x * y wrt argument 1 aka y."""
     if not isinstance(x, Arr):
         x = np.array(x)
     return multiply_back0(grad_out, out, y, x)
@@ -82,14 +87,15 @@ if MAIN:
     tests.test_multiply_back(multiply_back0, multiply_back1)
     tests.test_multiply_back_float(multiply_back0, multiply_back1)
 
+
 # %%
 def forward_and_back(a: Arr, b: Arr, c: Arr) -> Tuple[Arr, Arr, Arr]:
-    '''
+    """
     Calculates the output of the computational graph above (g), then backpropogates the gradients and returns dg/da, dg/db, and dg/dc
-    '''
-    d = a*b
+    """
+    d = a * b
     e = np.log(c)
-    f = d*e
+    f = d * e
     g = np.log(f)
 
     grad = np.array([1])
@@ -103,16 +109,16 @@ def forward_and_back(a: Arr, b: Arr, c: Arr) -> Tuple[Arr, Arr, Arr]:
     return grad_a, grad_b, grad_c
 
 
-
 if MAIN:
     tests.test_forward_and_back(forward_and_back)
 
 
 ##### PART 2: Autograd
 
+
 @dataclass(frozen=True)
 class Recipe:
-    '''Extra information necessary to run backpropagation. You don't need to modify this.'''
+    """Extra information necessary to run backpropagation. You don't need to modify this."""
 
     func: Callable
     "The 'inner' NumPy function that does the actual forward computation."
@@ -134,32 +140,35 @@ class BackwardFuncLookup:
     def __init__(self) -> None:
         self.data = {}
 
-    def add_back_func(self, forward_fn: Callable, arg_position: int, back_fn: Callable) -> None:
+    def add_back_func(
+        self, forward_fn: Callable, arg_position: int, back_fn: Callable
+    ) -> None:
         self.data[forward_fn, arg_position] = back_fn
 
     def get_back_func(self, forward_fn: Callable, arg_position: int) -> Callable:
         return self.data[forward_fn, arg_position]
 
 
-if MAIN:
-    BACK_FUNCS = BackwardFuncLookup()
-    BACK_FUNCS.add_back_func(np.log, 0, log_back)
-    BACK_FUNCS.add_back_func(np.multiply, 0, multiply_back0)
-    BACK_FUNCS.add_back_func(np.multiply, 1, multiply_back1)
 
-    assert BACK_FUNCS.get_back_func(np.log, 0) == log_back
-    assert BACK_FUNCS.get_back_func(np.multiply, 0) == multiply_back0
-    assert BACK_FUNCS.get_back_func(np.multiply, 1) == multiply_back1
+BACK_FUNCS = BackwardFuncLookup()
+BACK_FUNCS.add_back_func(np.log, 0, log_back)
+BACK_FUNCS.add_back_func(np.multiply, 0, multiply_back0)
+BACK_FUNCS.add_back_func(np.multiply, 1, multiply_back1)
 
-    print("Tests passed - BackwardFuncLookup class is working as expected!")
+assert BACK_FUNCS.get_back_func(np.log, 0) == log_back
+assert BACK_FUNCS.get_back_func(np.multiply, 0) == multiply_back0
+assert BACK_FUNCS.get_back_func(np.multiply, 1) == multiply_back1
+
+# print("Tests passed - BackwardFuncLookup class is working as expected!")
 
 # %%
 Arr = np.ndarray
 
+
 class Tensor:
-    '''
+    """
     A drop-in replacement for torch.Tensor supporting a subset of features.
-    '''
+    """
 
     array: Arr
     "The underlying array. Can be shared between multiple Tensors."
@@ -289,40 +298,46 @@ class Tensor:
 
     @property
     def is_leaf(self):
-        '''Same as https://pytorch.org/docs/stable/generated/torch.Tensor.is_leaf.html'''
+        """Same as https://pytorch.org/docs/stable/generated/torch.Tensor.is_leaf.html"""
         if self.requires_grad and self.recipe and self.recipe.parents:
             return False
         return True
 
     def __bool__(self):
         if np.array(self.shape).prod() != 1:
-            raise RuntimeError("bool value of Tensor with more than one value is ambiguous")
+            raise RuntimeError(
+                "bool value of Tensor with more than one value is ambiguous"
+            )
         return bool(self.item())
 
+
 def empty(*shape: int) -> Tensor:
-    '''Like torch.empty.'''
+    """Like torch.empty."""
     return Tensor(np.empty(shape))
 
+
 def zeros(*shape: int) -> Tensor:
-    '''Like torch.zeros.'''
+    """Like torch.zeros."""
     return Tensor(np.zeros(shape))
 
+
 def arange(start: int, end: int, step=1) -> Tensor:
-    '''Like torch.arange(start, end).'''
+    """Like torch.arange(start, end)."""
     return Tensor(np.arange(start, end, step=step))
 
+
 def tensor(array: Arr, requires_grad=False) -> Tensor:
-    '''Like torch.tensor.'''
+    """Like torch.tensor."""
     return Tensor(array, requires_grad=requires_grad)
 
 
 # %%
 def log_forward(x: Tensor) -> Tensor:
-    '''Performs np.log on a Tensor object.'''
+    """Performs np.log on a Tensor object."""
     track = grad_tracking_enabled and (x.requires_grad or x.recipe is not None)
     out = Tensor(np.log(x.array), requires_grad=track)
     if track:
-        recipe = Recipe(np.log, (x.array,), kwargs={}, parents={0 : x})
+        recipe = Recipe(np.log, (x.array,), kwargs={}, parents={0: x})
         out.recipe = recipe
     return out
 
@@ -335,8 +350,13 @@ if MAIN:
     grad_tracking_enabled = False
     b = log_forward(a)
     grad_tracking_enabled = True
-    assert not b.requires_grad, "should not require grad if grad tracking globally disabled"
-    assert b.recipe is None, "should not create recipe if grad tracking globally disabled"
+    assert (
+        not b.requires_grad
+    ), "should not require grad if grad tracking globally disabled"
+    assert (
+        b.recipe is None
+    ), "should not create recipe if grad tracking globally disabled"
+
 
 # %%
 def multiply_forward(a: Union[Tensor, int], b: Union[Tensor, int]) -> Tensor:
@@ -348,7 +368,7 @@ def multiply_forward(a: Union[Tensor, int], b: Union[Tensor, int]) -> Tensor:
     track = grad_tracking_enabled and (a_requires_grad or b_requires_grad)
     a_arg = a if a_is_int else a.array
     b_arg = b if b_is_int else b.array
-    prod = a_arg*b_arg
+    prod = a_arg * b_arg
     out = Tensor(prod, requires_grad=track)
     if track:
         recipe = Recipe(np.multiply, (a_arg, b_arg), kwargs={}, parents={})
@@ -370,7 +390,196 @@ if MAIN:
     grad_tracking_enabled = False
     b = multiply_forward(a, b)
     grad_tracking_enabled = True
-    assert not b.requires_grad, "should not require grad if grad tracking globally disabled"
-    assert b.recipe is None, "should not create recipe if grad tracking globally disabled"
+    assert (
+        not b.requires_grad
+    ), "should not require grad if grad tracking globally disabled"
+    assert (
+        b.recipe is None
+    ), "should not create recipe if grad tracking globally disabled"
+
 
 # %%
+def wrap_forward_fn(numpy_func: Callable, is_differentiable=True) -> Callable:
+    """
+    numpy_func: Callable
+        takes any number of positional arguments, some of which may be NumPy arrays, and
+        any number of keyword arguments which we aren't allowing to be NumPy arrays at
+        present. It returns a single NumPy array.
+
+    is_differentiable:
+        if True, numpy_func is differentiable with respect to some input argument, so we
+        may need to track information in a Recipe. If False, we definitely don't need to
+        track information.
+
+    Return: Callable
+        It has the same signature as numpy_func, except wherever there was a NumPy array,
+        this has a Tensor instead.
+    """
+
+    def to_data(x: Union[Tensor, int, float]):
+        if isinstance(x, Tensor):
+            return x.array
+        return x
+
+    def tensor_func(*args: Any, **kwargs: Any) -> Tensor:
+        assert not any(isinstance(kwarg, Tensor) for kwarg in kwargs.values())
+
+        any_input_requires_grad = any(
+            isinstance(arg, Tensor) and arg.requires_grad
+            for arg in args
+        )
+        track = grad_tracking_enabled and any_input_requires_grad and is_differentiable
+
+        args_to_arrays = [to_data(arg) for arg in args]
+        out_array = numpy_func(*args_to_arrays, **kwargs)
+        out = Tensor(out_array, requires_grad=track)
+
+        if track:
+            parents = {
+                idx: arg
+                for idx, arg in enumerate(args)
+                if isinstance(arg, Tensor) and arg.requires_grad
+            }
+            out.recipe = Recipe(numpy_func, args_to_arrays, kwargs, parents)
+
+        return out
+
+    return tensor_func
+
+
+def _sum(x: Arr, dim=None, keepdim=False) -> Arr:
+    # need to be careful with sum, because kwargs have different names in torch and numpy
+    return np.sum(x, axis=dim, keepdims=keepdim)
+
+
+if MAIN:
+    log = wrap_forward_fn(np.log)
+    multiply = wrap_forward_fn(np.multiply)
+    eq = wrap_forward_fn(np.equal, is_differentiable=False)
+    sum = wrap_forward_fn(_sum)
+
+    tests.test_log(Tensor, log)
+    tests.test_log_no_grad(Tensor, log)
+    tests.test_multiply(Tensor, multiply)
+    tests.test_multiply_no_grad(Tensor, multiply)
+    tests.test_multiply_float(Tensor, multiply)
+    tests.test_sum(Tensor)
+
+#%%
+class Node:
+    def __init__(self, *children):
+        self.children = list(children)
+
+
+def get_children(node: Node) -> List[Node]:
+    return node.children
+
+
+def topological_sort(node: Node, get_children: Callable) -> List[Node]:
+    '''
+    Return a list of node's descendants in reverse topological order from future to past (i.e. `node` should be last).
+
+    Should raise an error if the graph with `node` as root is not in fact acyclic.
+    '''
+    # SOLUTION
+
+    result: List[Node] = [] # stores the list of nodes to be returned (in reverse topological order)
+    perm: set[Node] = set() # same as `result`, but as a set (faster to check for membership)
+    temp: set[Node] = set() # keeps track of previously visited nodes (to detect cyclicity)
+
+    def visit(cur: Node):
+        '''
+        Recursive function which visits all the children of the current node, and appends them all
+        to `result` in the order they were found.
+        '''
+        if cur in perm:
+            return
+        if cur in temp:
+            raise ValueError("Not a DAG!")
+        temp.add(cur)
+
+        for next in get_children(cur):
+            visit(next)
+
+        result.append(cur)
+        perm.add(cur)
+        temp.remove(cur)
+
+    visit(node)
+    return result
+
+#%%
+def sorted_computational_graph(tensor: Tensor) -> List[Tensor]:
+    '''
+    For a given tensor, return a list of Tensors that make up the nodes of the given Tensor's computational graph,
+    in reverse topological order (i.e. `tensor` should be first).
+    '''
+    def get_children(x: Tensor) -> List[Tensor]:
+        if x.recipe is None:
+            return []
+        return x.recipe.parents.values()
+
+    return topological_sort(tensor, get_children)[::-1]
+
+
+
+if MAIN:
+    a = Tensor([1], requires_grad=True)
+    b = Tensor([2], requires_grad=True)
+    c = Tensor([3], requires_grad=True)
+    d = a * b
+    e = c.log()
+    f = d * e
+    g = f.log()
+    name_lookup = {a: "a", b: "b", c: "c", d: "d", e: "e", f: "f", g: "g"}
+
+    print([name_lookup[t] for t in sorted_computational_graph(g)])
+
+
+#%%
+def backprop(end_node: Tensor, end_grad: Optional[Tensor] = None) -> None:
+    '''Accumulates gradients in the grad field of each leaf node.
+
+    tensor.backward() is equivalent to backprop(tensor).
+
+    end_node:
+        The rightmost node in the computation graph.
+        If it contains more than one element, end_grad must be provided.
+    end_grad:
+        A tensor of the same shape as end_node.
+        Set to 1 if not specified and end_node has only one element.
+    '''
+
+    if end_grad is None:
+        assert np.prod(end_node.shape) == 1
+        end_grad = Tensor([1.])
+    end_node.grad = end_grad
+
+    order = sorted_computational_graph(end_node)
+
+    for tensor in order:
+        if tensor.recipe is None:
+            continue
+        for idx, arg in tensor.recipe.parents.items():
+            backward_function = BACK_FUNCS.get_back_func(tensor.recipe.func, idx)
+            grad = Tensor(backward_function(
+                tensor.grad.array,
+                tensor.array,
+                *tensor.recipe.args,
+                **tensor.recipe.kwargs,
+            ))
+            if arg.grad is None:
+                arg.grad = grad
+            else:
+                arg.grad = arg.grad
+
+        if not (tensor.is_leaf and tensor.requires_grad):
+            tensor.grad = None
+
+
+
+if MAIN:
+    tests.test_backprop(Tensor)
+    tests.test_backprop_branching(Tensor)
+    tests.test_backprop_requires_grad_false(Tensor)
+    tests.test_backprop_float_arg(Tensor)
