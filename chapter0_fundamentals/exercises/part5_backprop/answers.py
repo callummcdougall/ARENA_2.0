b6aefbb458483ece70a362c4e99f8da46d57e479
ARENA Account <pranav@arena.education>
2023-05-26 10:04:45 +0000

    start

---
+++ chapter0_fundamentals/exercises/part5_backprop/answers.py
@@ -0,0 +1,76 @@
+# %%
+from IPython import get_ipython
+
+ipython = get_ipython()
+
+ipython.run_line_magic("load_ext", "autoreload")
+
+ipython.run_line_magic("autoreload", "2")
+
+# %%
+import os
+import sys
+import re
+import time
+import torch as t
+import numpy as np
+from pathlib import Path
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any, Callable, Iterator, Iterable, Optional, Union, Dict, List, Tuple
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+Arr = np.ndarray
+grad_tracking_enabled = True
+
+# Make sure exercises are in the path
+chapter = r"chapter0_fundamentals"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part5_backprop"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+os.chdir(section_dir)
+
+import part5_backprop.tests as tests
+from part5_backprop.utils import visualize, get_mnist
+from plotly_utils import line
+
+MAIN = __name__ == "__main__"
+# %%
+def log_back(grad_out: Arr, out: Arr, x: Arr) -> Arr:
+    '''Backwards function for f(x) = log(x)
+
+    grad_out: Gradient of some loss wrt out
+    out: the output of np.log(x).
+    x: the input of np.log.
+
+    Return: gradient of the given loss wrt x
+    '''
+    return grad_out / x
+
+
+if MAIN:
+    tests.test_log_back(log_back)
+# %%
+def unbroadcast(broadcasted: Arr, original: Arr) -> Arr:
+    '''
+    Sum 'broadcasted' until it has the shape of 'original'.
+
+    broadcasted: An array that was formerly of the same shape of 'original' and was expanded by broadcasting rules.
+    '''
+    n_dims_to_sum = len(broadcasted.shape) - len(original.shape)
+    broadcasted = broadcasted.sum(axis=tuple(range(n_dims_to_sum)))
+
+    # Step 2: sum over dims which were originally 1 (but don't remove them)
+    dims_to_sum = tuple([
+        i for i, (o, b) in enumerate(zip(original.shape, broadcasted.shape))
+        if o == 1 and b > 1
+    ])
+    broadcasted = broadcasted.sum(axis=dims_to_sum, keepdims=True)
+
+    return broadcasted
+
+
+if MAIN:
+    tests.test_unbroadcast(unbroadcast)
+# %%
