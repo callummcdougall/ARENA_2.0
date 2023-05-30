75e5a89a74320893ecbd2ab528d4f42960049948
ARENA Account <pranav@arena.education>
2023-05-30 14:18:00 +0000

    attention investigation

--- chapter1_transformers/exercises/part2_intro_to_mech_interp/answers.py
+++ chapter1_transformers/exercises/part2_intro_to_mech_interp/answers.py
@@ -119,4 +119,163 @@ if MAIN:
         attention=attention_pattern,
         attention_head_names=[f"L0H{i}" for i in range(12)],
     ))
+
+#
+#
+# Induction Heads
+#
+#
+
+# %%
+
+if MAIN:
+    cfg = HookedTransformerConfig(
+        d_model=768,
+        d_head=64,
+        n_heads=12,
+        n_layers=2,
+        n_ctx=2048,
+        d_vocab=50278,
+        attention_dir="causal",
+        attn_only=True, # defaults to False
+        tokenizer_name="EleutherAI/gpt-neox-20b", 
+        seed=398,
+        use_attn_result=True,
+        normalization_type=None, # defaults to "LN", i.e. layernorm with weights & biases
+        positional_embedding_type="shortformer"
+    )
+
+
+# %%
+if MAIN:
+    weights_dir = (section_dir / "attn_only_2L_half.pth").resolve()
+
+    if not weights_dir.exists():
+        url = "https://drive.google.com/uc?id=1vcZLJnJoYKQs-2KOjkd6LvHZrkSdoxhu"
+        output = str(weights_dir)
+        gdown.download(url, output)
+# %%
+if MAIN:
+    model = HookedTransformer(cfg)
+    pretrained_weights = t.load(weights_dir, map_location=device)
+    model.load_state_dict(pretrained_weights)
+# %%
+
+if MAIN:
+    text = "We think that powerful, significantly superhuman machine intelligence is more likely than not to be created this century. If current machine learning techniques were scaled up to this level, we think they would by default produce systems that are deceptive or manipulative, and that no solid plans are known for how to avoid this."
+
+    logits, cache = model.run_with_cache(text, remove_batch_dim=True)
+    token_str = model.to_str_tokens(text)
+    
+    for j in range(model.cfg.n_layers):
+        attn_patterns = cache["pattern", j, "attn"]
+        print(attn_patterns.shape)
+
+        display(cv.attention.attention_patterns(
+            tokens=token_str, 
+            attention=attn_patterns,
+            attention_head_names=[f"L{j}H{i}" for i in range(12)],
+        ))
+
+# %%
+
+print
+
+
+
+
+# %%
+def current_attn_detector(cache: ActivationCache) -> List[str]:
+    '''
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be current-token heads
+    '''
+    threshold = 0.3
+    result = []
+    for layer in range(2):
+        for head in range(12):
+            pattern = cache['pattern', layer][head]
+            if t.trace(pattern) / t.sum(pattern) > threshold:
+                result.append(f"{layer}.{head}")
+    return result
+
+def prev_attn_detector(cache: ActivationCache) -> List[str]:
+    '''
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be prev-token heads
+    '''
+    threshold = 0.3
+    result = []
+    for layer in range(2):
+        for head in range(12):
+            pattern = cache['pattern', layer][head]
+            if t.sum(t.diagonal(pattern, offset=-1)) / t.sum(pattern) > threshold:
+                result.append(f"{layer}.{head}")
+    return result
+
+def first_attn_detector(cache: ActivationCache) -> List[str]:
+    '''
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be first-token heads
+    '''
+    threshold = 0.5
+    result = []
+    for layer in range(2):
+        for head in range(12):
+            pattern = cache['pattern', layer][head]
+            if t.sum(pattern[:,0]) / t.sum(pattern) > threshold:
+                result.append(f"{layer}.{head}")
+    return result
+
+
+if MAIN:
+    print("Heads attending to current token  = ", ", ".join(current_attn_detector(cache)))
+    print("Heads attending to previous token = ", ", ".join(prev_attn_detector(cache)))
+    print("Heads attending to first token    = ", ", ".join(first_attn_detector(cache)))
+    
+
+# %%
+
+def generate_repeated_tokens(
+    model: HookedTransformer, seq_len: int, batch: int = 1
+) -> Int[Tensor, "batch full_seq_len"]:
+    '''
+    Generates a sequence of repeated random tokens
+
+    Outputs are:
+        rep_tokens: [batch, 1+2*seq_len]
+    '''
+    random_seq = t.randint(0, model.cfg.d_vocab, size=(batch, seq_len))
+    prefix = t.ones((batch,1), dtype=t.int) * model.tokenizer.bos_token_id
+    prompt = t.cat((prefix, random_seq, random_seq), dim=-1)
+    return prompt
+
+
+def run_and_cache_model_repeated_tokens(model: HookedTransformer, seq_len: int, batch: int = 1) -> Tuple[t.Tensor, t.Tensor, ActivationCache]:
+    '''
+    Generates a sequence of repeated random tokens, and runs the model on it, returning logits, tokens and cache
+
+    Should use the `generate_repeated_tokens` function above
+
+    Outputs are:
+        rep_tokens: [batch, 1+2*seq_len]
+        rep_logits: [batch, 1+2*seq_len, d_vocab]
+        rep_cache: The cache of the model run on rep_tokens
+    '''
+    semi_rand_prompt_id = generate_repeated_tokens(model=model, seq_len=seq_len, batch=batch)
+    semi_rand_prompt_str = model.to_str_tokens(semi_rand_prompt_id)
+    logits, cache = model.run_with_cache(semi_rand_prompt_str)
+    return semi_rand_prompt_id, logits, cache
+
+
+if MAIN:
+    seq_len = 50
+    batch = 1
+    (rep_tokens, rep_logits, rep_cache) = run_and_cache_model_repeated_tokens(model, seq_len, batch)
+    rep_cache.remove_batch_dim()
+    rep_str = model.to_str_tokens(rep_tokens)
+    model.reset_hooks()
+    log_probs = get_log_probs(rep_logits, rep_tokens).squeeze()
+
+    print(f"Performance on the first half: {log_probs[:seq_len].mean():.3f}")
+    print(f"Performance on the second half: {log_probs[seq_len:].mean():.3f}")
+
+    plot_loss_difference(log_probs, rep_str, seq_len)
 # %%
