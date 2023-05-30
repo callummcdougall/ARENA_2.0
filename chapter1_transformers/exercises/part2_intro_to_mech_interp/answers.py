5d00dc65c7dcdebb1aa03e8ff280ae1f8a76e42c
ARENA Account <pranav@arena.education>
2023-05-30 16:30:23 +0000

    playing with hooks

--- chapter1_transformers/exercises/part2_intro_to_mech_interp/answers.py
+++ chapter1_transformers/exercises/part2_intro_to_mech_interp/answers.py
@@ -460,3 +460,128 @@ loss = model.run_with_hooks(
     fwd_hooks=[("blocks.1.attn.hook_pattern", hook_function)],
 )
 # %%
+
+if MAIN:
+    seq_len = 50
+    batch = 10
+    rep_tokens_10 = generate_repeated_tokens(model, seq_len, batch)
+
+    # We make a tensor to store the induction score for each head.
+    # We put it on the model's device to avoid needing to move things between the GPU and CPU, which can be slow.
+    induction_score_store = t.zeros(
+        (model.cfg.n_layers, model.cfg.n_heads), device=model.cfg.device
+    )
+
+
+def induction_score_hook(
+    pattern: Float[Tensor, "batch head_index dest_pos source_pos"],
+    hook: HookPoint,
+):
+    """
+    Calculates the induction score, and stores it in the [layer, head] position of the `induction_score_store` tensor.
+    """
+    offset = -(seq_len - 1)
+
+    ave_p = pattern.mean(dim=0)
+    for h, head in enumerate(ave_p):
+        ind_score = head.diagonal(offset=offset).mean()
+        induction_score_store[hook.layer(), h] = ind_score
+
+
+if MAIN:
+    pattern_hook_names_filter = lambda name: name.endswith("pattern")
+
+    # Run with hooks (this is where we write to the `induction_score_store` tensor`)
+    model.run_with_hooks(
+        rep_tokens_10,
+        return_type=None,  # For efficiency, we don't need to calculate the logits
+        fwd_hooks=[(pattern_hook_names_filter, induction_score_hook)],
+    )
+
+    # Plot the induction scores for each head in each layer
+    imshow(
+        induction_score_store,
+        labels={"x": "Head", "y": "Layer"},
+        title="Induction Score by Head",
+        text_auto=".2f",
+        width=900,
+        height=400,
+    )
+
+# %%
+
+
+def visualize_pattern_hook(
+    pattern: Float[Tensor, "batch head_index dest_pos source_pos"],
+    hook: HookPoint,
+):
+    print("Layer: ", hook.layer())
+    display(
+        cv.attention.attention_patterns(
+            tokens=gpt2_small.to_str_tokens(rep_tokens[0]), attention=pattern.mean(0)
+        )
+    )
+
+
+def make_induction_hook(score_store):
+    def induction_score_hook(
+        pattern: Float[Tensor, "batch head_index dest_pos source_pos"],
+        hook: HookPoint,
+    ):
+        """
+        Calculates the induction score, and stores it in the [layer, head] position of the `induction_score_store` tensor.
+        """
+        offset = -(seq_len - 1)
+
+        ave_p = pattern.mean(dim=0)
+        for h, head in enumerate(ave_p):
+            ind_score = head.diagonal(offset=offset).mean()
+            score_store[hook.layer(), h] = ind_score
+
+    return induction_score_hook
+
+
+if MAIN:
+    # YOUR CODE HERE - find induction heads in gpt2_small
+    pattern_hook_names_filter = lambda name: name.endswith("pattern")
+
+    store = t.zeros(
+        gpt2_small.cfg.n_layers, gpt2_small.cfg.n_heads, device=model.cfg.device
+    )
+    hook = make_induction_hook(store)
+
+    # Run with hooks (this is where we write to the `induction_score_store` tensor`)
+    gpt2_small.run_with_hooks(
+        rep_tokens_10,
+        return_type=None,  # For efficiency, we don't need to calculate the logits
+        fwd_hooks=[(pattern_hook_names_filter, hook)],
+    )
+
+    # Plot the induction scores for each head in each layer
+    imshow(
+        store,
+        labels={"x": "Head", "y": "Layer"},
+        title="Induction Score by Head",
+        text_auto=".2f",
+        width=900,
+        height=400,
+    )
+
+    gpt2_small.run_with_hooks(
+        rep_tokens,
+        return_type=None,  # For efficiency, we don't need to calculate the logits
+        fwd_hooks=[(lambda n: "pattern" in n and "5" in n, visualize_pattern_hook)],
+    )
+
+    # out, cache = gpt2_small.run_with_cache(rep_tokens_10[0])
+    # pattern = cache["pattern", 5]
+
+    # display(
+    #     cv.attention.attention_patterns(
+    #         tokens=gpt2_small.to_str_tokens(rep_tokens_10[0]),
+    #         attention=pattern.mean(0)
+    #     )
+    # )
+
+
+# %%
