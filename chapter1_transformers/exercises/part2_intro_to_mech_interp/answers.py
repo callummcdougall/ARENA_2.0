0b2143f28b139db2862ec5145d9c737b49b8b6d0
ARENA Account <pranav@arena.education>
2023-05-31 09:09:23 +0000

    Direct logit attribution

--- chapter1_transformers/exercises/part2_intro_to_mech_interp/answers.py
+++ chapter1_transformers/exercises/part2_intro_to_mech_interp/answers.py
@@ -572,16 +572,105 @@ if MAIN:
         return_type=None,  # For efficiency, we don't need to calculate the logits
         fwd_hooks=[(lambda n: "pattern" in n and "5" in n, visualize_pattern_hook)],
     )
-
-    # out, cache = gpt2_small.run_with_cache(rep_tokens_10[0])
-    # pattern = cache["pattern", 5]
-
-    # display(
-    #     cv.attention.attention_patterns(
-    #         tokens=gpt2_small.to_str_tokens(rep_tokens_10[0]),
-    #         attention=pattern.mean(0)
-    #     )
-    # )
+# %%
 
 
+def logit_attribution(
+    embed: Float[Tensor, "seq d_model"],
+    l1_results: Float[Tensor, "seq nheads d_model"],
+    l2_results: Float[Tensor, "seq nheads d_model"],
+    W_U: Float[Tensor, "d_model d_vocab"],
+    tokens: Int[Tensor, "seq"],
+) -> Float[Tensor, "seq-1 n_components"]:
+    """
+    Inputs:
+        embed: the embeddings of the tokens (i.e. token + position embeddings)
+        l1_results: the outputs of the attention heads at layer 1 (with head as one of the dimensions)
+        l2_results: the outputs of the attention heads at layer 2 (with head as one of the dimensions)
+        W_U: the unembedding matrix
+        tokens: the token ids of the sequence
+
+    Returns:
+        Tensor of shape (seq_len-1, n_components)
+        represents the concatenation (along dim=-1) of logit attributions from:
+            the direct path (seq-1,1)
+            layer 0 logits (seq-1, n_heads)
+            layer 1 logits (seq-1, n_heads)
+        so n_components = 1 + 2*n_heads
+    """
+    W_U_correct_tokens = W_U[:, tokens[1:]]
+    embed_component = einops.einsum(
+        embed[:-1, :], W_U_correct_tokens, "seq d_model, d_model seq -> seq"
+    )
+    l1_component = einops.einsum(
+        l1_results[:-1, :, :],
+        W_U_correct_tokens,
+        "seq nheads d_model, d_model seq -> seq nheads",
+    )
+    l2_component = einops.einsum(
+        l2_results[:-1, :, :],
+        W_U_correct_tokens,
+        "seq nheads d_model, d_model seq -> seq nheads",
+    )
+    return t.concat(
+        (t.unsqueeze(embed_component, dim=-1), l1_component, l2_component), dim=-1
+    )
+
+
+if MAIN:
+    text = "We think that powerful, significantly superhuman machine intelligence is more likely than not to be created this century. If current machine learning techniques were scaled up to this level, we think they would by default produce systems that are deceptive or manipulative, and that no solid plans are known for how to avoid this."
+    # import random
+    # text = ''.join(random.choice("abcdefghigjlmnopqrstuvwxyz    ") for i in range(50)) * 2
+    logits, cache = model.run_with_cache(text, remove_batch_dim=True)
+    str_tokens = model.to_str_tokens(text)
+    tokens = model.to_tokens(text)
+
+    with t.inference_mode():
+        embed = cache["embed"]
+        l1_results = cache["result", 0]
+        l2_results = cache["result", 1]
+        logit_attr = logit_attribution(
+            embed, l1_results, l2_results, model.W_U, tokens[0]
+        )
+        # Uses fancy indexing to get a len(tokens[0])-1 length tensor, where the kth entry is the predicted logit for the correct k+1th token
+        correct_token_logits = logits[0, t.arange(len(tokens[0]) - 1), tokens[0, 1:]]
+        t.testing.assert_close(
+            logit_attr.sum(1), correct_token_logits, atol=1e-3, rtol=0
+        )
+        print("Tests passed!")
+
+# %%
+if MAIN:
+    embed = cache["embed"]
+    l1_results = cache["result", 0]
+    l2_results = cache["result", 1]
+    logit_attr = logit_attribution(embed, l1_results, l2_results, model.W_U, tokens[0])
+
+    plot_logit_attribution(model, logit_attr, tokens)
+# %%
+if MAIN:
+    seq_len = 50
+
+    embed = rep_cache["embed"]
+    l1_results = rep_cache["result", 0]
+    l2_results = rep_cache["result", 1]
+    first_half_tokens = rep_tokens[0, : 1 + seq_len]
+    second_half_tokens = rep_tokens[0, seq_len:]
+
+    # YOUR CODE HERE - define `first_half_logit_attr` and `second_half_logit_attr`
+    assert first_half_logit_attr.shape == (seq_len, 2 * model.cfg.n_heads + 1)
+    assert second_half_logit_attr.shape == (seq_len, 2 * model.cfg.n_heads + 1)
+
+    plot_logit_attribution(
+        model,
+        first_half_logit_attr,
+        first_half_tokens,
+        "Logit attribution (first half of repeated sequence)",
+    )
+    plot_logit_attribution(
+        model,
+        second_half_logit_attr,
+        second_half_tokens,
+        "Logit attribution (second half of repeated sequence)",
+    )
 # %%
