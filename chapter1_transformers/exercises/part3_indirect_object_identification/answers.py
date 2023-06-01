1ecf061cf3d1b9ea4938d60a53a6dbcd8fc05d1f
ARENA Account <pranav@arena.education>
2023-06-01 17:25:02 +0000

    ioi exploration

--- chapter1_transformers/exercises/part3_indirect_object_identification/answers.py
+++ chapter1_transformers/exercises/part3_indirect_object_identification/answers.py
@@ -22,6 +22,7 @@ from pathlib import Path
 from transformer_lens.hook_points import HookPoint
 from transformer_lens import utils, HookedTransformer, ActivationCache
 from transformer_lens.components import Embed, Unembed, LayerNorm, MLP
+from transformer_lens import patching
 
 t.set_grad_enabled(False)
 
@@ -37,3 +38,355 @@ import part3_indirect_object_identification.tests as tests
 device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
 
 MAIN = __name__ == "__main__"
+
+
+# %%
+if MAIN:
+    model = HookedTransformer.from_pretrained(
+        "gpt2-small",
+        center_unembed=True,
+        center_writing_weights=True,
+        fold_ln=True,
+        refactor_factored_attn_matrices=True,
+    )
+
+
+# %%
+# Here is where we test on a single prompt
+# Result: 70% probability on Mary, as we expect
+if MAIN:
+    example_prompt = "After John and Mary went to the store, John gave a bottle of milk to"
+    example_answer = " Mary"
+    utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
+
+
+# %%
+if MAIN:
+    prompt_format = [
+        "When John and Mary went to the shops,{} gave the bag to",
+        "When Tom and James went to the park,{} gave the ball to",
+        "When Dan and Sid went to the shops,{} gave an apple to",
+        "After Martin and Amy went to the park,{} gave a drink to",
+    ]
+    name_pairs = [
+        (" Mary", " John"),
+        (" Tom", " James"),
+        (" Dan", " Sid"),
+        (" Martin", " Amy"),
+    ]
+
+    # Define 8 prompts, in 4 groups of 2 (with adjacent prompts having answers swapped)
+    prompts = [
+        prompt.format(name) 
+        for (prompt, names) in zip(prompt_format, name_pairs) for name in names[::-1] 
+    ]
+    # Define the answers for each prompt, in the form (correct, incorrect)
+    answers = [names[::i] for names in name_pairs for i in (1, -1)]
+    # Define the answer tokens (same shape as the answers)
+    answer_tokens = t.concat([
+        model.to_tokens(names, prepend_bos=False).T for names in answers
+    ])
+
+    rprint(prompts)
+    rprint(answers)
+    rprint(answer_tokens)
+
+
+# %%
+if MAIN:
+    table = Table("Prompt", "Correct", "Incorrect", title="Prompts & Answers:")
+
+    for prompt, answer in zip(prompts, answers):
+        table.add_row(prompt, repr(answer[0]), repr(answer[1]))
+
+    rprint(table)
+
+
+# %%
+if MAIN:
+    tokens = model.to_tokens(prompts, prepend_bos=True)
+    # Move the tokens to the GPU
+    tokens = tokens.to(device)
+    # Run the model and cache all activations
+    original_logits, cache = model.run_with_cache(tokens)
+
+
+# %%
+def logits_to_ave_logit_diff(
+    logits: Float[Tensor, "batch seq d_vocab"],
+    answer_tokens: Float[Tensor, "batch 2"] = answer_tokens,
+    per_prompt: bool = False
+):
+    '''
+    Returns logit difference between the correct and incorrect answer.
+
+    If per_prompt=True, return the array of differences rather than the average.
+    '''
+    logits = einops.rearrange(logits, "batch seq d_vocab -> seq batch d_vocab")
+    batch_idx = t.arange(answer_tokens.shape[0])
+    diffs = logits[-1, batch_idx, answer_tokens[:,0]] - logits[-1, batch_idx, answer_tokens[:,1]]#should be (batch, 2)
+    if per_prompt:
+        return diffs
+    return einops.reduce(diffs, "batch -> ", "mean")
+
+
+if MAIN:
+    tests.test_logits_to_ave_logit_diff(logits_to_ave_logit_diff)
+
+    original_per_prompt_diff = logits_to_ave_logit_diff(original_logits, answer_tokens, per_prompt=True)
+    print("Per prompt logit difference:", original_per_prompt_diff)
+    original_average_logit_diff = logits_to_ave_logit_diff(original_logits, answer_tokens)
+    print("Average logit difference:", original_average_logit_diff)
+
+    cols = [
+        "Prompt", 
+        Column("Correct", style="rgb(0,200,0) bold"), 
+        Column("Incorrect", style="rgb(255,0,0) bold"), 
+        Column("Logit Difference", style="bold")
+    ]
+    table = Table(*cols, title="Logit differences")
+
+    for prompt, answer, logit_diff in zip(prompts, answers, original_per_prompt_diff):
+        table.add_row(prompt, repr(answer[0]), repr(answer[1]), f"{logit_diff.item():.3f}")
+
+    rprint(table)
+
+
+# %%
+if MAIN:
+    answer_residual_directions: Float[Tensor, "batch 2 d_model"] = model.tokens_to_residual_directions(answer_tokens)
+    print("Answer residual directions shape:", answer_residual_directions.shape)
+
+    correct_residual_directions, incorrect_residual_directions = answer_residual_directions.unbind(dim=1)
+    logit_diff_directions: Float[Tensor, "batch d_model"] = correct_residual_directions - incorrect_residual_directions
+    print(f"Logit difference directions shape:", logit_diff_directions.shape)
+
+
+# %%
+# cache syntax - resid_post is the residual stream at the end of the layer, -1 gets the final layer. The general syntax is [activation_name, layer_index, sub_layer_type]. 
+
+if MAIN:
+    final_residual_stream: Float[Tensor, "batch seq d_model"] = cache["resid_post", -1]
+    print(f"Final residual stream shape: {final_residual_stream.shape}")
+    final_token_residual_stream: Float[Tensor, "batch d_model"] = final_residual_stream[:, -1, :]
+
+    # Apply LayerNorm scaling (to just the final sequence position)
+    # pos_slice is the subset of the positions we take - here the final token of each prompt
+    scaled_final_token_residual_stream = cache.apply_ln_to_stack(final_token_residual_stream, layer=-1, pos_slice=-1)
+
+    average_logit_diff = einops.einsum(
+        scaled_final_token_residual_stream, logit_diff_directions,
+        "batch d_model, batch d_model ->"
+    ) / len(prompts)
+
+    print(f"Calculated average logit diff: {average_logit_diff:.10f}")
+    print(f"Original logit difference:     {original_average_logit_diff:.10f}")
+
+    t.testing.assert_close(average_logit_diff, original_average_logit_diff)
+
+
+# %%
+def residual_stack_to_logit_diff(
+    residual_stack: Float[Tensor, "... batch d_model"], 
+    cache: ActivationCache,
+    logit_diff_directions: Float[Tensor, "batch d_model"] = logit_diff_directions,
+) -> Float[Tensor, "..."]:
+    '''
+    Gets the avg logit difference between the correct and incorrect answer for a given 
+    stack of components in the residual stream.
+    '''
+    normed_resid_stack: Float[Tensor, "... batch d_model"] = cache.apply_ln_to_stack(residual_stack, layer=-1, pos_slice=-1)
+    return einops.einsum(logit_diff_directions, normed_resid_stack, "batch d_model, ... batch d_model -> ...")/logit_diff_directions.shape[0]
+
+
+if MAIN:
+    t.testing.assert_close(
+        residual_stack_to_logit_diff(final_token_residual_stream, cache),
+        original_average_logit_diff
+    )
+
+
+# %%
+if MAIN:
+    accumulated_residual, labels = cache.accumulated_resid(layer=-1, incl_mid=True, pos_slice=-1, return_labels=True)
+    # accumulated_residual has shape (component, batch, d_model)
+
+    logit_lens_logit_diffs: Float[Tensor, "component"] = residual_stack_to_logit_diff(accumulated_residual, cache)
+
+    line(
+        logit_lens_logit_diffs, 
+        hovermode="x unified",
+        title="Logit Difference From Accumulated Residual Stream",
+        labels={"x": "Layer", "y": "Logit Diff"},
+        xaxis_tickvals=labels,
+        width=800
+    )
+
+
+# %%
+if MAIN:
+    per_layer_residual, labels = cache.decompose_resid(layer=-1, pos_slice=-1, return_labels=True)
+    per_layer_logit_diffs = residual_stack_to_logit_diff(per_layer_residual, cache)
+
+    line(
+        per_layer_logit_diffs, 
+        hovermode="x unified",
+        title="Logit Difference From Each Layer",
+        labels={"x": "Layer", "y": "Logit Diff"},
+        xaxis_tickvals=labels,
+        width=800
+    )
+
+
+# %%
+if MAIN:
+    per_head_residual, labels = cache.stack_head_results(layer=-1, pos_slice=-1, return_labels=True)
+    per_head_residual = einops.rearrange(
+        per_head_residual, 
+        "(layer head) ... -> layer head ...", 
+        layer=model.cfg.n_layers
+    )
+    per_head_logit_diffs = residual_stack_to_logit_diff(per_head_residual, cache)
+
+    imshow(
+        per_head_logit_diffs, 
+        labels={"x":"Head", "y":"Layer"}, 
+        title="Logit Difference From Each Head",
+        width=600
+    )
+
+
+# %%
+def topk_of_Nd_tensor(tensor: Float[Tensor, "rows cols"], k: int):
+    '''
+    Helper function: does same as tensor.topk(k).indices, but works over 2D tensors.
+    Returns a list of indices, i.e. shape [k, tensor.ndim].
+
+    Example: if tensor is 2D array of values for each head in each layer, this will
+    return a list of heads.
+    '''
+    i = t.topk(tensor.flatten(), k).indices
+    return np.array(np.unravel_index(utils.to_numpy(i), tensor.shape)).T.tolist()
+
+
+
+if MAIN:
+    k = 3
+
+    for head_type in ["Positive", "Negative"]:
+
+        # Get the heads with largest (or smallest) contribution to the logit difference
+        top_heads = topk_of_Nd_tensor(per_head_logit_diffs * (1 if head_type=="Positive" else -1), k)
+
+        # Get all their attention patterns
+        attn_patterns_for_important_heads: Float[Tensor, "head q k"] = t.stack([
+            cache["pattern", layer][:, head].mean(0)
+            for layer, head in top_heads
+        ])
+
+        # Display results
+        display(HTML(f"<h2>Top {k} {head_type} Logit Attribution Heads</h2>"))
+        display(cv.attention.attention_heads(
+            attention = attn_patterns_for_important_heads,
+            tokens = model.to_str_tokens(tokens[0]),
+            attention_head_names = [f"{layer}.{head}" for layer, head in top_heads],
+        ))
+
+
+# %%
+if MAIN:
+    clean_tokens = tokens
+    # Swap each adjacent pair to get corrupted tokens
+    indices = [i+1 if i % 2 == 0 else i-1 for i in range(len(tokens))]
+    corrupted_tokens = clean_tokens[indices]
+
+    print(
+        "Clean string 0:    ", model.to_string(clean_tokens[0]), "\n"
+        "Corrupted string 0:", model.to_string(corrupted_tokens[0])
+    )
+
+    clean_logits, clean_cache = model.run_with_cache(clean_tokens)
+    corrupted_logits, corrupted_cache = model.run_with_cache(corrupted_tokens)
+
+    clean_logit_diff = logits_to_ave_logit_diff(clean_logits, answer_tokens)
+    print(f"Clean logit diff: {clean_logit_diff:.4f}")
+
+    corrupted_logit_diff = logits_to_ave_logit_diff(corrupted_logits, answer_tokens)
+    print(f"Corrupted logit diff: {corrupted_logit_diff:.4f}")
+
+
+# %%
+def ioi_metric(
+    logits: Float[Tensor, "batch seq d_vocab"], 
+    answer_tokens: Float[Tensor, "batch 2"] = answer_tokens,
+    corrupted_logit_diff: float = corrupted_logit_diff,
+    clean_logit_diff: float = clean_logit_diff,
+) -> Float[Tensor, ""]:
+    '''
+    Linear function of logit diff, calibrated so that it equals 0 when performance is 
+    same as on corrupted input, and 1 when performance is same as on clean input.
+    '''
+    double_diff = clean_logit_diff - corrupted_logit_diff
+    mixed_diff = logits_to_ave_logit_diff(logits, answer_tokens)
+    return t.tensor((mixed_diff + double_diff/2)/double_diff)
+
+
+if MAIN:
+    t.testing.assert_close(ioi_metric(clean_logits).item(), 1.0)
+    t.testing.assert_close(ioi_metric(corrupted_logits).item(), 0.0)
+    t.testing.assert_close(ioi_metric((clean_logits + corrupted_logits) / 2).item(), 0.5)
+
+
+# %%
+if MAIN:
+    act_patch_resid_pre = patching.get_act_patch_resid_pre(
+        model = model,
+        corrupted_tokens = corrupted_tokens,
+        clean_cache = clean_cache,
+        patching_metric = ioi_metric
+    )
+
+    labels = [f"{tok} {i}" for i, tok in enumerate(model.to_str_tokens(clean_tokens[0]))]
+
+    imshow(
+        act_patch_resid_pre, 
+        labels={"x": "Position", "y": "Layer"},
+        x=labels,
+        title="resid_pre Activation Patching",
+        width=600
+    )
+
+
+# %%
+def patch_residual_component(
+    corrupted_residual_component: Float[Tensor, "batch pos d_model"],
+    hook: HookPoint, 
+    pos: int, 
+    clean_cache: ActivationCache
+) -> Float[Tensor, "batch pos d_model"]:
+    '''
+    Patches a given sequence position in the residual stream, using the value
+    from the clean cache.
+    '''
+    clean_cache[]
+
+def get_act_patch_resid_pre(
+    model: HookedTransformer, 
+    corrupted_tokens: Float[Tensor, "batch pos"], 
+    clean_cache: ActivationCache, 
+    patching_metric: Callable[[Float[Tensor, "batch pos d_vocab"]], float]
+) -> Float[Tensor, "layer pos"]:
+    '''
+    Returns an array of results of patching each position at each layer in the residual
+    stream, using the value from the clean cache.
+
+    The results are calculated using the patching_metric function, which should be
+    called on the model's logit output.
+    '''
+    pass
+
+
+if MAIN:
+    act_patch_resid_pre_own = get_act_patch_resid_pre(model, corrupted_tokens, clean_cache, ioi_metric)
+
+    t.testing.assert_close(act_patch_resid_pre, act_patch_resid_pre_own)
