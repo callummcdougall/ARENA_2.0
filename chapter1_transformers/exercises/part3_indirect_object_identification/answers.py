3521234eefc893e216022573803d567c90bfb06c
ARENA Account <pranav@arena.education>
2023-06-01 10:21:05 +0000

    ioi

---
+++ chapter1_transformers/exercises/part3_indirect_object_identification/answers.py
@@ -0,0 +1,39 @@
+# %%
+import os; os.environ["ACCELERATE_DISABLE_RICH"] = "1"
+import sys
+from pathlib import Path
+import torch as t
+from torch import Tensor
+import numpy as np
+import einops
+from tqdm.notebook import tqdm
+import plotly.express as px
+import webbrowser
+import re
+import itertools
+from jaxtyping import Float, Int, Bool
+from typing import List, Optional, Callable, Tuple, Dict, Literal, Set
+from functools import partial
+from IPython.display import display, HTML
+from rich.table import Table, Column
+from rich import print as rprint
+import circuitsvis as cv
+from pathlib import Path
+from transformer_lens.hook_points import HookPoint
+from transformer_lens import utils, HookedTransformer, ActivationCache
+from transformer_lens.components import Embed, Unembed, LayerNorm, MLP
+
+t.set_grad_enabled(False)
+
+# Make sure exercises are in the path
+chapter = r"chapter1_transformers"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = (exercises_dir / "part3_indirect_object_identification").resolve()
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+from plotly_utils import imshow, line, scatter, bar
+import part3_indirect_object_identification.tests as tests
+
+device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
+
+MAIN = __name__ == "__main__"
