f587347ea7ef0f087ba27bc1a212f7f3ff865738
ARENA Account <pranav@arena.education>
2023-06-08 12:40:31 +0000

    initial commit

---
+++ chapter2_rl/exercises/part1_intro_to_rl/answers.py
@@ -0,0 +1,36 @@
+
+#%%
+!pip install gym==0.23.1
+!pip install pygame
+
+import os
+import sys
+from typing import Optional, Union, List, Tuple
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import random
+from tqdm import tqdm
+import einops
+from pathlib import Path
+import matplotlib.pyplot as plt
+import gym
+import gym.envs.registration
+import gym.spaces
+
+Arr = np.ndarray
+max_episode_steps = 1000
+N_RUNS = 200
+
+# Make sure exercises are in the path
+chapter = r"chapter2_rl"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part1_intro_to_rl"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+import part1_intro_to_rl.utils as utils
+import part1_intro_to_rl.tests as tests
+from plotly_utils import imshow
+
+MAIN = __name__ == "__main__"
+# %%
