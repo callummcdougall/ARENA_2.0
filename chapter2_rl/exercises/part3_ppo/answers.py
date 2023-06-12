e84e071614a42ca4f2e347064a5d68aab8783c8a
ARENA Account <pranav@arena.education>
2023-06-12 15:51:42 +0100

    implement learning phase

--- chapter2_rl/exercises/part3_ppo/answers.py
+++ chapter2_rl/exercises/part3_ppo/answers.py
@@ -365,3 +365,110 @@ class PPOAgent(nn.Module):
 
 
 tests.test_ppo_agent(PPOAgent)
+
+# %%
+def calc_clipped_surrogate_objective(
+    probs: Categorical, 
+    mb_action: Int[Tensor, "minibatch_size"], 
+    mb_advantages: Float[Tensor, "minibatch_size"], 
+    mb_logprobs: Float[Tensor, "minibatch_size"], 
+    clip_coef: float, 
+    eps: float = 1e-8
+) -> Float[Tensor, ""]:
+    '''Return the clipped surrogate objective, suitable for maximisation with gradient ascent.
+
+    probs:
+        a distribution containing the actor's unnormalized logits of shape (minibatch_size, num_actions)
+    mb_action:
+        what actions actions were taken in the sampled minibatch
+    mb_advantages:
+        advantages calculated from the sampled minibatch
+    mb_logprobs:
+        logprobs of the actions taken in the sampled minibatch (according to the old policy)
+    clip_coef:
+        amount of clipping, denoted by epsilon in Eq 7.
+    eps:
+        used to add to std dev of mb_advantages when normalizing (to avoid dividing by zero)
+    '''
+    assert mb_action.shape == mb_advantages.shape == mb_logprobs.shape 
+    pi_current = probs.log_prob(mb_action)
+
+    pi_ratio = t.exp(pi_current - mb_logprobs)
+
+    clipped_term = np.clip(pi_ratio, 1 - clip_coef, 1 + clip_coef)
+
+    mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + eps)
+    
+    loss = t.min(pi_ratio * mb_advantages, clipped_term * mb_advantages).mean()
+
+    return loss
+
+
+
+    
+
+
+
+tests.test_calc_clipped_surrogate_objective(calc_clipped_surrogate_objective)
+# %%
+
+def calc_value_function_loss(
+    values: Float[Tensor, "minibatch_size"],
+    mb_returns: Float[Tensor, "minibatch_size"],
+    vf_coef: float
+) -> Float[Tensor, ""]:
+    '''Compute the value function portion of the loss function.
+
+    values:
+        the value function predictions for the sampled minibatch (using the updated critic network)
+    mb_returns:
+        the target for our updated critic network (computed as `advantages + values` from the old network)
+    vf_coef:
+        the coefficient for the value loss, which weights its contribution to the overall loss. Denoted by c_1 in the paper.
+    '''
+    assert values.shape == mb_returns.shape
+    return vf_coef * 0.5 * t.nn.MSELoss()(values, mb_returns)
+
+
+tests.test_calc_value_function_loss(calc_value_function_loss)
+# %%
+
+def calc_entropy_bonus(probs: Categorical, ent_coef: float):
+    '''Return the entropy bonus term, suitable for gradient ascent.
+
+    probs:
+        the probability distribution for the current policy
+    ent_coef: 
+        the coefficient for the entropy loss, which weights its contribution to the overall objective function. Denoted by c_2 in the paper.
+    '''
+    return ent_coef * probs.entropy().mean()
+
+
+tests.test_calc_entropy_bonus(calc_entropy_bonus)
+# %%
+class PPOScheduler:
+    def __init__(self, optimizer: Optimizer, initial_lr: float, end_lr: float, total_training_steps: int):
+        self.optimizer = optimizer
+        self.initial_lr = initial_lr
+        self.end_lr = end_lr
+        self.total_training_steps = total_training_steps
+        self.n_step_calls = 0
+
+    def step(self):
+        '''Implement linear learning rate decay so that after total_training_steps calls to step, the learning rate is end_lr.
+        '''
+        self.n_step_calls += 1
+        lr = self.initial_lr - min(1, (self.n_step_calls / self.total_training_steps)) * (self.initial_lr + self.end_lr)
+        for param_group in self.optimizer.param_groups: 
+            param_group['lr'] = lr
+
+
+def make_optimizer(agent: PPOAgent, total_training_steps: int, initial_lr: float, end_lr: float) -> Tuple[optim.Adam, PPOScheduler]:
+    '''Return an appropriately configured Adam with its attached scheduler.'''
+    optimizer = optim.Adam(agent.parameters(), lr=initial_lr, eps=1e-5, maximize=True)
+    scheduler = PPOScheduler(optimizer, initial_lr, end_lr, total_training_steps)
+    return (optimizer, scheduler)
+
+
+tests.test_ppo_scheduler(PPOScheduler)
+# %%
