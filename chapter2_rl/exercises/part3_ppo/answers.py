392be33216e7ab67c61867c34d262d8890a55080
Rusheb <rusheb.shah@gmail.com>
2023-06-12 13:06:55 +0100

    Implement advantage calculation
    Co-authored by Jan Betley <jan.betley@gmail.com>

---
+++ chapter2_rl/exercises/part3_ppo/answers.py
@@ -0,0 +1,167 @@
+#  %%
+import os
+os.environ["ACCELERATE_DISABLE_RICH"] = "1"
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+import random
+import time
+import sys
+import re
+import pandas as pd
+from dataclasses import dataclass
+from tqdm import tqdm
+import numpy as np
+from numpy.random import Generator
+import plotly.express as px
+import torch as t
+from torch import Tensor
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import Dataset
+import gym
+from gym.envs.classic_control.cartpole import CartPoleEnv
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.categorical import Categorical
+from gym.spaces import Discrete
+import einops
+import copy
+from pathlib import Path
+from typing import List, Tuple, Dict, Any, Union, Callable, Optional
+from jaxtyping import Float, Int, Bool
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger, CSVLogger
+import wandb
+
+# Make sure exercises are in the path
+chapter = r"chapter2_rl"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part3_ppo"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+from part1_intro_to_rl.utils import make_env
+from part2_dqn.utils import set_global_seeds
+from part2_dqn.solutions import Probe1, Probe2, Probe3, Probe4, Probe5
+import part3_ppo.utils as utils
+import part3_ppo.tests as tests
+from plotly_utils import plot_cartpole_obs_and_dones
+
+# Register our probes from last time
+for idx, probe in enumerate([Probe1, Probe2, Probe3, Probe4, Probe5]):
+    gym.envs.registration.register(id=f"Probe{idx+1}-v0", entry_point=probe)
+
+Arr = np.ndarray
+
+device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+MAIN = __name__ == "__main__"
+# %%
+@dataclass
+class PPOArgs:
+    exp_name: str = "PPO_Implementation"
+    seed: int = 1
+    cuda: bool = t.cuda.is_available()
+    log_dir: str = "logs"
+    use_wandb: bool = False
+    wandb_project_name: str = "PPOCart"
+    wandb_entity: str = None
+    capture_video: bool = True
+    env_id: str = "CartPole-v1"
+    total_timesteps: int = 500000
+    learning_rate: float = 0.00025
+    num_envs: int = 4
+    num_steps: int = 128
+    gamma: float = 0.99
+    gae_lambda: float = 0.95
+    num_minibatches: int = 4
+    batches_per_epoch: int = 4
+    clip_coef: float = 0.2
+    ent_coef: float = 0.01
+    vf_coef: float = 0.5
+    max_grad_norm: float = 0.5
+    batch_size: int = 512
+    minibatch_size: int = 128
+
+    def __post_init__(self):
+        assert self.batch_size % self.minibatch_size == 0, "batch_size must be divisible by minibatch_size"
+        self.total_epochs = self.total_timesteps // (self.num_steps * self.num_envs)
+        self.total_training_steps = self.total_epochs * self.batches_per_epoch * (self.batch_size // self.minibatch_size)
+
+
+
+args = PPOArgs(minibatch_size=256)
+utils.arg_help(args)
+# %%
+def layer_init(layer: nn.Linear, std=np.sqrt(2), bias_const=0.0):
+    t.nn.init.orthogonal_(layer.weight, std)
+    t.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+
+def get_actor_and_critic(envs: gym.vector.SyncVectorEnv) -> Tuple[nn.Module, nn.Module]:
+    '''
+    Returns (actor, critic), the networks used for PPO.
+    '''
+    obs_shape = envs.single_observation_space.shape
+    num_obs = np.array(obs_shape).prod()
+    num_actions = envs.single_action_space.n
+    
+    actor = nn.Sequential(
+        layer_init(nn.Linear(num_obs, 64)),
+        nn.Tanh(),
+        layer_init(nn.Linear(64, 64)),
+        nn.Tanh(),
+        layer_init(nn.Linear(64, num_actions), std=0.01),
+    )
+    
+    critic = nn.Sequential(
+        layer_init(nn.Linear(num_obs, 64)),
+        nn.Tanh(),
+        layer_init(nn.Linear(64, 64)),
+        nn.Tanh(),
+        layer_init(nn.Linear(64, 1), std=1),
+    )
+
+    return actor, critic
+
+tests.test_get_actor_and_critic(get_actor_and_critic)
+
+# %%
+@t.inference_mode()
+def compute_advantages(
+    next_value: t.Tensor,
+    next_done: t.Tensor,
+    rewards: t.Tensor,
+    values: t.Tensor,
+    dones: t.Tensor,
+    gamma: float,
+    gae_lambda: float,
+) -> t.Tensor:
+    '''Compute advantages using Generalized Advantage Estimation.
+    next_value: shape (env,)
+    next_done: shape (env,)
+    rewards: shape (buffer_size, env)
+    values: shape (buffer_size, env)
+    dones: shape (buffer_size, env)
+    Return: shape (buffer_size, env)
+    '''
+    advs = t.empty_like(values)       
+    n_steps = rewards.shape[0]
+    
+    last_gaelam = 0
+    for t_ in reversed(range(n_steps)):
+        if t_ == n_steps - 1:
+            nextnonterminal = 1.0 - next_done
+            nextvalues = next_value
+        else:
+            nextnonterminal = 1.0 - dones[t_ + 1]
+            nextvalues = values[t_ + 1]
+        
+        delta = rewards[t_] + gamma * nextvalues * nextnonterminal - values[t_]
+        advs[t_] = last_gaelam = delta + gamma * gae_lambda * nextnonterminal * last_gaelam
+        
+    return advs
+
+
+tests.test_compute_advantages(compute_advantages)
+# %%
