c1cab312bd6ec9fc593b14cf6343c1502d367935
TommyBark <tomas.turlik@gmail.com>
2023-06-15 08:45:01 +0000

    Add lambda startup script

---
+++ lambda_script.sh
@@ -0,0 +1,8 @@
+pip install plotly
+pip install wandb
+pip install pytorch-lightning
+pip install jaxtyping
+pip install einops
+pip install git+https://github.com/callummcdougall/CircuitsVis.git#subdirectory=python
+pip install transformer_lens
+pip install protobuf==3.20.*
