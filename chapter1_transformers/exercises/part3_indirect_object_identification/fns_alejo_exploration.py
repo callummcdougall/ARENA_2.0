9f5c5f4e3753e382053ddacafbc5a79f9ba9e2d9
ARENA Account <pranav@arena.education>
2023-06-16 09:12:55 +0000

    just saving

---
+++ chapter1_transformers/exercises/part3_indirect_object_identification/fns_alejo_exploration.py
@@ -0,0 +1,151 @@
+# %%
+
+
+import os; os.environ["ACCELERATE_DISABLE_RICH"] = "1"
+import sys
+from pathlib import Path
+import torch as t
+from torch import Tensor
+import numpy as np
+import einops
+from tqdm.notebook import tqdm
+import plotly.express as px
+import webbrowser
+import re
+import itertools
+from jaxtyping import Float, Int, Bool
+from typing import List, Optional, Callable, Tuple, Dict, Literal, Set, Union
+from functools import partial
+from IPython.display import display, HTML
+from rich.table import Table, Column
+from rich import print as rprint
+import circuitsvis as cv
+from pathlib import Path
+from transformer_lens.hook_points import HookPoint
+from transformer_lens import utils, HookedTransformer, ActivationCache
+from transformer_lens.components import Embed, Unembed, LayerNorm, MLP
+
+t.set_grad_enabled(False)
+
+# Make sure exercises are in the path
+chapter = r"chapter1_transformers"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = (exercises_dir / "part3_indirect_object_identification").resolve()
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+from plotly_utils import imshow, line, scatter, bar
+import part3_indirect_object_identification.tests as tests
+
+device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
+
+
+# %%
+
+
+def logits_to_ave_logit_diff(
+    logits: Float[Tensor, "batch seq d_vocab"],
+    answer_tokens: Float[Tensor, "batch 2"],
+    per_prompt: bool = False
+):
+    '''
+    Returns logit difference between the correct and incorrect answer.
+
+    If per_prompt=True, return the array of differences rather than the average.
+    '''
+    # Only the final logits are relevant for the answer
+    final_logits: Float[Tensor, "batch d_vocab"] = logits[:, -1, :]
+    # Get the logits corresponding to the indirect object / subject tokens respectively
+    answer_logits: Float[Tensor, "batch 2"] = final_logits.gather(dim=-1, index=answer_tokens)
+    # Find logit difference
+    correct_logits, incorrect_logits = answer_logits.unbind(dim=-1)
+    answer_logit_diff = correct_logits - incorrect_logits
+    return answer_logit_diff if per_prompt else answer_logit_diff.mean()
+
+def ioi_metric(
+    logits: Float[Tensor, "batch seq d_vocab"], 
+    answer_tokens: Float[Tensor, "batch 2"],
+    corrupted_logit_diff: float,
+    clean_logit_diff: float,
+) -> Float[Tensor, ""]:
+    '''
+    Linear function of logit diff, calibrated so that it equals 0 when performance is 
+    same as on corrupted input, and 1 when performance is same as on clean input.
+    '''
+    patched_logit_diff = logits_to_ave_logit_diff(logits, answer_tokens)
+    return (patched_logit_diff - corrupted_logit_diff) / (clean_logit_diff  - corrupted_logit_diff)
+
+
+
+# %% PATCHING PLOTS
+
+from transformer_lens import patching
+
+def plot_patching_experiments(clean_tokens: Float[Tensor, "batch seq"],
+                              corrupted_tokens: Float[Tensor, "batch seq"],
+                              answer_tokens: Float[Tensor, "batch 2"],
+                              model: HookedTransformer,
+    ) -> None:
+
+
+    clean_logits, clean_cache = model.run_with_cache(clean_tokens)
+    corrupted_logits, corrupted_cache = model.run_with_cache(corrupted_tokens)
+
+    clean_logit_diff = logits_to_ave_logit_diff(clean_logits, answer_tokens)
+    corrupted_logit_diff = logits_to_ave_logit_diff(corrupted_logits, answer_tokens)
+    print(f'{clean_logit_diff = }', f'{corrupted_logit_diff = }', sep='\n')
+
+    ioi_metric_arged = partial(ioi_metric, answer_tokens=answer_tokens,
+                               corrupted_logit_diff=corrupted_logit_diff, clean_logit_diff=clean_logit_diff)
+    
+    act_patch_block_every = patching.get_act_patch_block_every(model, corrupted_tokens, clean_cache, ioi_metric_arged)
+    
+    act_patch_attn_head_out_all_pos = patching.get_act_patch_attn_head_out_all_pos(
+        model, 
+        corrupted_tokens, 
+        clean_cache, 
+        ioi_metric_arged
+    )
+
+    act_patch_attn_head_all_pos_every = patching.get_act_patch_attn_head_all_pos_every(
+        model, 
+        corrupted_tokens, 
+        clean_cache, 
+        ioi_metric_arged
+    )
+
+    labels = [f"{tok} {i}" for i, tok in enumerate(model.to_str_tokens(clean_tokens[0]))]
+    
+    imshow(
+        act_patch_block_every,
+        x=labels, 
+        facet_col=0, # This argument tells plotly which dimension to split into separate plots
+        facet_labels=["Residual Stream", "Attn Output", "MLP Output"], # Subtitles of separate plots
+        title="Logit Difference From Patched Attn Head Output", 
+        labels={"x": "Sequence Position", "y": "Layer"},
+        width=1000,
+    )
+
+    imshow(
+        act_patch_attn_head_out_all_pos, 
+        labels={"y": "Layer", "x": "Head"}, 
+        title="attn_head_out Activation Patching (All Pos)",
+        width=600
+    )
+
+    imshow(
+        act_patch_attn_head_all_pos_every, 
+        facet_col=0, 
+        facet_labels=["Output", "Query", "Key", "Value", "Pattern"],
+        title="Activation Patching Per Head (All Pos)", 
+        labels={"x": "Head", "y": "Layer"},
+    )
+
+# %%
+
+def print_predictions_from_prompt(prompt: str, model: HookedTransformer, top_k: int = 5) -> None:
+    tokens = model.to_tokens(prompt)
+    logits = model(tokens)
+    probs = t.softmax(logits, dim=-1)
+    top_probs, top_words = probs.topk(k=top_k, dim=-1)
+    for top_prob, top_word in zip(top_probs, top_words):
+        print(f"{model.to_str_tokens(top_word)}: {100*top_prob:.3f}")
