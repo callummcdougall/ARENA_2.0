fd6d8acff3736b2a51b941332ae01e52e1444f4d
Vinay Hiremath <github@vinayh.com>
2023-06-16 18:00:45 +0100

    Added incomplete impl of gather_tree

--- chapter3_distributed/answers.py
+++ chapter3_distributed/answers.py
@@ -2,7 +2,7 @@
 import torch
 from torch import distributed as dist
 from torch.distributed import ReduceOp
-from typing import Callable
+from typing import List
 
 import threading
 
@@ -128,17 +128,15 @@ if __name__ == '__main__':
 from test import test_allreduce_butterfly
 
 def allreduce_butterfly(tensor: torch.Tensor, op=ReduceOp.SUM):
-    # TODO: does not produce consistent results in tensor
-    print(f'INIT {dist.get_rank()} {tensor}')
     rank = bin(dist.get_rank())[2:].zfill(len(bin(dist.get_world_size()-1)[2:]))
     buff = torch.empty_like(tensor)
     for i in range(len(rank)):
         partner_rank = rank[:i] + str(1-int(rank[i])) + rank[i+1:]
         partner_rank = int(partner_rank, 2)
-        dist.send(tensor, partner_rank)
+        dist.send(tensor.clone(), partner_rank)
         dist.recv(buff, partner_rank)
         if op == ReduceOp.SUM:
-                tensor += buff
+            tensor += buff
         elif op == ReduceOp.PRODUCT:
             tensor *= buff
         elif op == ReduceOp.MAX:
@@ -148,9 +146,49 @@ def allreduce_butterfly(tensor: torch.Tensor, op=ReduceOp.SUM):
         else:
             raise NotImplementedError(f'op {op} not implemented')
     dist.barrier()
-    print(f'FINAL {dist.get_rank()} {tensor}')
 
 if __name__ == '__main__':
     test_allreduce_butterfly(allreduce_butterfly)
 
+# %%
+from test import test_gather_tree
+
+def gather_tree(tensor: torch.Tensor, dst: int):
+    def indir_ranks(curr_mult: int) -> List[int]:
+        indir_rank = 0
+        out = [indir_rank]
+        indir_mult = curr_mult
+        while indir_rank + indir_mult < dist.get_world_size():
+            indir_rank += indir_mult
+            out.append(int(indir_rank))
+            indir_mult *= 2
+        return out
+    # TODO: Have changed code for tmp_buff to receive full buffer from src rank, need to debug extraction of indices
+    # and sending to identify deadlock
+    curr_mult = dist.get_world_size() // 2
+    rank_shifted = lambda: (dist.get_rank() - dst) % dist.get_world_size()
+    buff = torch.zeros([tensor.shape[0] * dist.get_world_size()] + list(tensor.shape[1:]))
+    b_idx = lambda x: int(tensor.shape[0] * x)
+    buff[b_idx(dist.get_rank()):b_idx(dist.get_rank()+1)] = tensor
+    while curr_mult >= 1:
+        if rank_shifted() < curr_mult:
+            print(f'rank {dist.get_rank()} receiving, curr_mult {curr_mult}, addns {indir_ranks(curr_mult)}')
+            src_rank = int((dist.get_rank()+curr_mult)%dist.get_world_size())
+            tmp_buff = torch.empty_like(buff)
+            dist.recv(tmp_buff, src_rank)
+            for r in indir_ranks(curr_mult):
+                data_src_rank = (src_rank+r)%dist.get_world_size()
+                print(f'rank {src_rank} to {dist.get_rank()} - recv idx {b_idx(data_src_rank)}:{b_idx(data_src_rank+1)}')
+                buff[b_idx(data_src_rank):b_idx(data_src_rank+1)] = tmp_buff[b_idx(data_src_rank):b_idx(data_src_rank+1)]
+                print(f'rank {src_rank} to {dist.get_rank()} - DONE recv idx {b_idx(data_src_rank)}:{b_idx(data_src_rank+1)} - {buff}')
+        elif rank_shifted() < curr_mult * 2:
+            print(f'rank {dist.get_rank()} sending')
+            for r in indir_ranks(curr_mult):
+                dst_rank = int((dist.get_rank()-curr_mult)%dist.get_world_size())
+                dist.send(buff, dst_rank)
+        curr_mult //= 2
+    dist.barrier()
+
+if __name__ == '__main__':
+    test_gather_tree(gather_tree)
 # %%
--- chapter3_distributed/test.py
+++ chapter3_distributed/test.py
@@ -365,7 +365,6 @@ def test_reduce_naive(reduce_impl: Callable):
     assert all((i != dst_rank and len(fake_dist.reads_by[i]) == 0) or (i == dst_rank and len(fake_dist.reads_by[i]) == fake_dist.world_size-1) for i in range(fake_dist.world_size))
     assert all((i != dst_rank and len(fake_dist.writes_from[i]) == 1) or (i == dst_rank and len(fake_dist.writes_from[i]) == 0) for i in range(fake_dist.world_size))
 
-
 def test_reduce_tree(reduce_impl: Callable):
     dst_rank = 7
     world_size = 8
@@ -401,3 +400,32 @@ def test_allreduce_butterfly(allreduce_impl: Callable):
             partner_rank = rank[:i] + str(1 - int(rank[i])) + rank[i + 1:]
             partner_rank = int(partner_rank, 2)
             assert partner_rank in v
+
+def test_gather_tree(gather_impl: Callable):
+    dst_rank = 0
+    world_size = 8
+    assert is_power_of_two(world_size), 'world_size must be power of two'
+    fake_dist = test_scaffold(gather_impl, lambda x: torch.Tensor([x]), [dst_rank], world_size=world_size) # world_size = power of 2
+    for i in range(fake_dist.world_size):
+        print('rank', i, 'reads by', len(fake_dist.reads_by[i]), 'writes from', len(fake_dist.writes_from[i]))
+    # assert all(len(fake_dist.writes_from[i]) == 1 for i in range(fake_dist.world_size) if i != dst_rank)
+    # assert all(len(fake_dist.reads_by[i]) == math.ceil(math.log(fake_dist.world_size, 2)) for i in range(fake_dist.world_size) if i == dst_rank)
+
+def test_allgather_butterfly(allgather_impl: Callable):
+    world_size = 8
+    assert is_power_of_two(world_size), 'world_size must be power of two'
+    fake_dist = test_scaffold(allgather_impl, lambda x: torch.Tensor([x]), [], world_size=world_size) # world_size = power of 2
+    assert all(len(fake_dist.reads_by[i]) == math.ceil(math.log(fake_dist.world_size, 2)) for i in range(fake_dist.world_size))
+    assert all(len(fake_dist.writes_from[i]) == math.ceil(math.log(fake_dist.world_size, 2)) for i in range(fake_dist.world_size))
+    # for k, v in fake_dist.writes_from.items():
+    #     rank = bin(k)[2:].zfill(len(bin(fake_dist.world_size - 1)[2:]))
+    #     for i in range(len(rank)):
+    #         partner_rank = rank[:i] + str(1 - int(rank[i])) + rank[i + 1:]
+    #         partner_rank = int(partner_rank, 2)
+    #         assert partner_rank in v
+    # for k, v in fake_dist.reads_by.items():
+    #     rank = bin(k)[2:].zfill(len(bin(fake_dist.world_size - 1)[2:]))
+    #     for i in range(len(rank)):
+    #         partner_rank = rank[:i] + str(1 - int(rank[i])) + rank[i + 1:]
+    #         partner_rank = int(partner_rank, 2)
+    #         assert partner_rank in v
