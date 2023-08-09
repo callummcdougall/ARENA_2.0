fe6ad92c73241a35583481efe62551c08930787d
callummcdougall <cal.s.mcdougall@gmail.com>
2023-08-09 14:35:12 +0100

    tabs to spaces

--- chapter1_transformers/instructions/pages/06_[1.6]_OthelloGPT.py
+++ chapter1_transformers/instructions/pages/06_[1.6]_OthelloGPT.py
@@ -2601,46 +2601,46 @@ Once you've finished implementing this function, you can run the code below. You
 
 ```python
 class LinearProbeTrainer:
-	def __init__(self, model: HookedTransformer, args: ProbeTrainingArgs):
-		super().__init__()
-		self.model = model
-		self.args = args
-		self.linear_probe = args.setup_linear_probe(model)
+    def __init__(self, model: HookedTransformer, args: ProbeTrainingArgs):
+        super().__init__()
+        self.model = model
+        self.args = args
+        self.linear_probe = args.setup_linear_probe(model)
 
-	def training_step(self, indices: Int[Tensor, "game_idx"]) -> t.Tensor:
+    def training_step(self, indices: Int[Tensor, "game_idx"]) -> t.Tensor:
 
-		# Get the game sequences and convert them to state stacks
-		games_int = board_seqs_int[indices.cpu()]
-		games_str = board_seqs_string[indices.cpu()]
-		state_stack = t.stack([t.tensor(seq_to_state_stack(game_str.tolist())) for game_str in games_str])
-		state_stack = state_stack[:, self.args.pos_start: self.args.pos_end, :, :]
-		state_stack_one_hot = state_stack_to_one_hot(state_stack).to(device)
-		batch_size = self.args.batch_size
-		game_len = self.args.length
+        # Get the game sequences and convert them to state stacks
+        games_int = board_seqs_int[indices.cpu()]
+        games_str = board_seqs_string[indices.cpu()]
+        state_stack = t.stack([t.tensor(seq_to_state_stack(game_str.tolist())) for game_str in games_str])
+        state_stack = state_stack[:, self.args.pos_start: self.args.pos_end, :, :]
+        state_stack_one_hot = state_stack_to_one_hot(state_stack).to(device)
+        batch_size = self.args.batch_size
+        game_len = self.args.length
 
-		# games_int = tensor of game sequences, each of length 60
-		# This is the input to our model
-		assert isinstance(games_int, Int[Tensor, f"batch={batch_size} full_game_len=60"])
+        # games_int = tensor of game sequences, each of length 60
+        # This is the input to our model
+        assert isinstance(games_int, Int[Tensor, f"batch={batch_size} full_game_len=60"])
 
-		# state_stack_one_hot = tensor of one-hot encoded states for each game
-		# We'll multiply this by our probe's estimated log probs along the `options` dimension, to get probe's estimated log probs for the correct option
-		assert isinstance(state_stack_one_hot, Int[Tensor, f"batch={batch_size} game_len={game_len} rows=8 cols=8 options=3"])
+        # state_stack_one_hot = tensor of one-hot encoded states for each game
+        # We'll multiply this by our probe's estimated log probs along the `options` dimension, to get probe's estimated log probs for the correct option
+        assert isinstance(state_stack_one_hot, Int[Tensor, f"batch={batch_size} game_len={game_len} rows=8 cols=8 options=3"])
 
         pass
 
-	def shuffle_training_indices(self):
-		'''
-		Returns the tensors you'll use to index into the training data.
-		'''
-		n_indices = self.args.num_games - (self.args.num_games % self.args.batch_size)
-		full_train_indices = t.randperm(self.args.num_games)[:n_indices]
-		full_train_indices = einops.rearrange(full_train_indices, "(batch_idx game_idx) -> batch_idx game_idx", game_idx=self.args.batch_size)
-		return full_train_indices
-		
+    def shuffle_training_indices(self):
+        '''
+        Returns the tensors you'll use to index into the training data.
+        '''
+        n_indices = self.args.num_games - (self.args.num_games % self.args.batch_size)
+        full_train_indices = t.randperm(self.args.num_games)[:n_indices]
+        full_train_indices = einops.rearrange(full_train_indices, "(batch_idx game_idx) -> batch_idx game_idx", game_idx=self.args.batch_size)
+        return full_train_indices
+        
 
-	def configure_optimizers(self):
-		optimizer = t.optim.AdamW([self.linear_probe], lr=self.args.lr, betas=self.args.betas, weight_decay=self.args.wd)
-		return optimizer
+    def configure_optimizers(self):
+        optimizer = t.optim.AdamW([self.linear_probe], lr=self.args.lr, betas=self.args.betas, weight_decay=self.args.wd)
+        return optimizer
 ```
 
 <details>
@@ -2649,72 +2649,72 @@ class LinearProbeTrainer:
 
 ```python
 class LinearProbeTrainer:
-	def __init__(self, model: HookedTransformer, args: ProbeTrainingArgs):
-		super().__init__()
-		self.model = model
-		self.args = args
-		self.linear_probe = args.setup_linear_probe(model)
+    def __init__(self, model: HookedTransformer, args: ProbeTrainingArgs):
+        super().__init__()
+        self.model = model
+        self.args = args
+        self.linear_probe = args.setup_linear_probe(model)
 
-	def training_step(self, indices: Int[Tensor, "game_idx"]) -> t.Tensor:
+    def training_step(self, indices: Int[Tensor, "game_idx"]) -> t.Tensor:
 
-		# Get the game sequences and convert them to state stacks
-		games_int = board_seqs_int[indices.cpu()]
-		games_str = board_seqs_string[indices.cpu()]
-		state_stack = t.stack([t.tensor(seq_to_state_stack(game_str.tolist())) for game_str in games_str])
-		state_stack = state_stack[:, self.args.pos_start: self.args.pos_end, :, :]
-		state_stack_one_hot = state_stack_to_one_hot(state_stack).to(device)
-		batch_size = self.args.batch_size
-		game_len = self.args.length
+        # Get the game sequences and convert them to state stacks
+        games_int = board_seqs_int[indices.cpu()]
+        games_str = board_seqs_string[indices.cpu()]
+        state_stack = t.stack([t.tensor(seq_to_state_stack(game_str.tolist())) for game_str in games_str])
+        state_stack = state_stack[:, self.args.pos_start: self.args.pos_end, :, :]
+        state_stack_one_hot = state_stack_to_one_hot(state_stack).to(device)
+        batch_size = self.args.batch_size
+        game_len = self.args.length
 
-		# games_int = tensor of game sequences, each of length 60
-		# This is the input to our model
-		assert isinstance(games_int, Int[Tensor, f"batch={batch_size} full_game_len=60"])
+        # games_int = tensor of game sequences, each of length 60
+        # This is the input to our model
+        assert isinstance(games_int, Int[Tensor, f"batch={batch_size} full_game_len=60"])
 
-		# state_stack_one_hot = tensor of one-hot encoded states for each game
-		# We'll multiply this by our probe's estimated log probs along the `options` dimension, to get probe's estimated log probs for the correct option
-		assert isinstance(state_stack_one_hot, Int[Tensor, f"batch={batch_size} game_len={game_len} rows=8 cols=8 options=3"])
+        # state_stack_one_hot = tensor of one-hot encoded states for each game
+        # We'll multiply this by our probe's estimated log probs along the `options` dimension, to get probe's estimated log probs for the correct option
+        assert isinstance(state_stack_one_hot, Int[Tensor, f"batch={batch_size} game_len={game_len} rows=8 cols=8 options=3"])
 
-		with t.inference_mode():
-			_, cache = model.run_with_cache(
-				games_int[:, :-1].to(device),
-				return_type=None,
-				names_filter=lambda name: name.endswith("resid_post")
-			)
-			resid_post = cache["resid_post", self.args.layer][:, self.args.pos_start: self.args.pos_end]
+        with t.inference_mode():
+            _, cache = model.run_with_cache(
+                games_int[:, :-1].to(device),
+                return_type=None,
+                names_filter=lambda name: name.endswith("resid_post")
+            )
+            resid_post = cache["resid_post", self.args.layer][:, self.args.pos_start: self.args.pos_end]
 
-		probe_out = einops.einsum(
-			resid_post,
-			self.linear_probe,
-			"batch pos d_model, modes d_model rows cols options -> modes batch pos rows cols options",
-		)
+        probe_out = einops.einsum(
+            resid_post,
+            self.linear_probe,
+            "batch pos d_model, modes d_model rows cols options -> modes batch pos rows cols options",
+        )
 
-		probe_log_probs = probe_out.log_softmax(-1)
-		probe_correct_log_probs = einops.reduce(
-			probe_log_probs * state_stack_one_hot,
-			"modes batch pos rows cols options -> modes pos rows cols",
-			"mean"
-		) * self.args.options # Multiply to correct for the mean over options
-		loss_even = -probe_correct_log_probs[0, 0::2].mean(0).sum() # note that "even" means odd in the game framing, since we offset by 5 moves lol
-		loss_odd = -probe_correct_log_probs[1, 1::2].mean(0).sum()
-		loss_all = -probe_correct_log_probs[2, :].mean(0).sum()
-		
-		loss = loss_even + loss_odd + loss_all
-		return loss
+        probe_log_probs = probe_out.log_softmax(-1)
+        probe_correct_log_probs = einops.reduce(
+            probe_log_probs * state_stack_one_hot,
+            "modes batch pos rows cols options -> modes pos rows cols",
+            "mean"
+        ) * self.args.options # Multiply to correct for the mean over options
+        loss_even = -probe_correct_log_probs[0, 0::2].mean(0).sum() # note that "even" means odd in the game framing, since we offset by 5 moves lol
+        loss_odd = -probe_correct_log_probs[1, 1::2].mean(0).sum()
+        loss_all = -probe_correct_log_probs[2, :].mean(0).sum()
+        
+        loss = loss_even + loss_odd + loss_all
+        return loss
 
 
-	def shuffle_training_indices(self):
-		'''
-		Returns the tensors you'll use to index into the training data.
-		'''
-		n_indices = self.args.num_games - (self.args.num_games % self.args.batch_size)
-		full_train_indices = t.randperm(self.args.num_games)[:n_indices]
-		full_train_indices = einops.rearrange(full_train_indices, "(batch_idx game_idx) -> batch_idx game_idx", game_idx=self.args.batch_size)
-		return full_train_indices
-		
+    def shuffle_training_indices(self):
+        '''
+        Returns the tensors you'll use to index into the training data.
+        '''
+        n_indices = self.args.num_games - (self.args.num_games % self.args.batch_size)
+        full_train_indices = t.randperm(self.args.num_games)[:n_indices]
+        full_train_indices = einops.rearrange(full_train_indices, "(batch_idx game_idx) -> batch_idx game_idx", game_idx=self.args.batch_size)
+        return full_train_indices
+        
 
-	def configure_optimizers(self):
-		optimizer = t.optim.AdamW([self.linear_probe], lr=self.args.lr, betas=self.args.betas, weight_decay=self.args.wd)
-		return optimizer
+    def configure_optimizers(self):
+        optimizer = t.optim.AdamW([self.linear_probe], lr=self.args.lr, betas=self.args.betas, weight_decay=self.args.wd)
+        return optimizer
 ```
 </details>
 
